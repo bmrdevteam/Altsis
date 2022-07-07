ee38c82dca3ef3c9b7d7d12719ac20e326815aeb
seedlessapple <luminousseedlessapple@gmail.com>
2022-07-07 11:32:53 +0900

    google login

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -6,14 +6,7 @@ type Props = {};
 const Home = (props: Props) => {
   return (
     <div>
-      <Form handleSubmit={(e:React.FormEvent<HTMLFormElement>)=>{
-        e.preventDefault()
-        console.log("submited")
-      }}>
-        <FormInput name="이세찬" placeholder="이름을 입력하시오" required={true} ></FormInput>
-        <FormSubmit/>
-
-      </Form>
+      
     </div>
   );
 };
