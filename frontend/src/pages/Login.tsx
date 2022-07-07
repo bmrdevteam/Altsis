2cce77466a59b160254d4db11d46cc87eabfae24
seedlessapple <luminousseedlessapple@gmail.com>
2022-07-07 09:16:34 +0900

    login

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,5 +1,10 @@
 import React, { useRef } from "react";
-import Form, { FormInput, FormSubmit } from "../components/UI/form/Form";
+import Form, {
+  FormColumn,
+  FormInput,
+  FormRow,
+  FormSubmit,
+} from "../components/UI/form/Form";
 import style from "../style/pages/login.module.scss";
 
 const Login = () => {
@@ -7,27 +12,34 @@ const Login = () => {
 
   console.log(usernameRef);
   return (
-    <div className={style.container}>
-      <Form
-        handleSubmit={(e: React.FormEvent<HTMLFormElement>) => {
-          e.preventDefault();
-          return console.log("s");
-        }}
-      >
-        <FormInput
-          name="username"
-          placeholder="username"
-          handleChange={(e: React.FormEvent<HTMLInputElement>) => {
-            console.log(usernameRef.current?.value);
+    <div className={style.section}>
+      <div className={style.container}>
+        <h1 className={style.title}>로그인</h1>
+        <Form
+          handleSubmit={(e: React.FormEvent<HTMLFormElement>) => {
+            e.preventDefault();
+            return console.log("s");
           }}
-          useRef={usernameRef}
-          required={true}
-        />
-        <FormInput name="이름" placeholder="name" />
-        <FormInput name="이메일" placeholder="email" />
-        <FormInput name="sdf" placeholder="password" />
-        <FormSubmit placeholder="로그인" />
-      </Form>
+        >
+          <FormColumn>
+            <FormInput
+              name="username"
+              placeholder="username"
+              handleChange={(e: React.FormEvent<HTMLInputElement>) => {
+                console.log(usernameRef.current?.value);
+              }}
+              useRef={usernameRef}
+              required={true}
+            />
+            <FormInput name="이름" placeholder="name" />
+          </FormColumn>
+          <FormColumn>
+            <FormInput name="이메일" placeholder="email" />
+            <FormInput name="sdf" placeholder="password" />
+          </FormColumn>
+          <FormSubmit placeholder="로그인" />
+        </Form>
+      </div>
     </div>
   );
 };
