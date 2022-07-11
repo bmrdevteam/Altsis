d0fc2c8366d2b0d777c20cab119f450b08fc34a1
seedlessapple <luminousseedlessapple@gmail.com>
2022-07-11 10:27:20 +0900

    Update Login.tsx

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -7,14 +7,18 @@ import Form, {
 } from "../components/UI/form/Form";
 import style from "../style/pages/login.module.scss";
 import axios from "axios";
+import Button from "../components/UI/button/Button";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const usernameRef = useRef<{ value: any }>();
   const passwordRef = useRef<{ value: any }>();
-
   const [errorMessage, setErrorMessage] = useState<string>("");
 
   //later hooks
+
+  //hooks
+  const navigate = useNavigate();
 
   useEffect(() => {
     return () => {
@@ -59,6 +63,9 @@ const Login = () => {
           setErrorMessage(errorMsg[i]?.msg);
 
           switch (errorMsg[i]?.msg) {
+            case "409":
+              //conflict
+              break;
             case "ID length error":
               console.log(1);
 
@@ -88,42 +95,64 @@ const Login = () => {
         <h1 className={style.title}>로그인</h1>
         <p className={style.error}>{errorMessage}</p>
         <Form handleSubmit={onLoginSubmit}>
-          {/* <FormColumn>
+          <FormColumn>
             <FormInput
               name="username"
-              placeholder="username"
+              placeholder="username or email"
               handleChange={(e: React.FormEvent<HTMLInputElement>) => {
                 console.log(usernameRef.current?.value);
               }}
               useRef={usernameRef}
               required={true}
             />
-            <FormInput name="이름" placeholder="name" required />
-          </FormColumn> */}
-          <FormColumn>
             <FormInput
-              name="username"
-              placeholder="username"
-              valueType="username"
-              required
-              useRef={usernameRef}
-            />
-            <FormInput
-              name="패스워드"
+              name="password"
               placeholder="password"
               valueType="password"
               required
-              useRef={passwordRef}
             />
           </FormColumn>
+
           <FormSubmit placeholder="로그인" />
         </Form>
-        <button onClick={()=>{
-          window.open("http://localhost:3000/api/user/google/login",'google로 로그인','width=430,height=500,location=no,status=no,scrollbars=yes')
-        }}>
-          google
-        </button>
-        
+        <div style={{ height: "4px" }}></div>
+        <Button
+          type="ghost"
+          handleClick={() => {
+            navigate("/register", { replace: true });
+          }}
+        >
+          회원가입
+        </Button>
+        {/* <Button
+          type="ghost"
+          handleClick={() => {
+            window.open(
+              "http://localhost:3000/api/user/google/login",
+              "google로 로그인",
+              "width=430,height=500,location=no,status=no,scrollbars=yes"
+            );
+          }}
+        >
+          google 로 로그인
+        </Button> */}
+        <div style={{ height: "4px" }}></div>
+
+        <div
+          id="g_id_onload"
+          data-client_id="628425542095-4l9d49h85pd0oapq5596964os4ongkei.apps.googleusercontent.com"
+          data-login_uri=""
+          data-auto_prompt="false"
+        ></div>
+        <div
+          className="g_id_signin"
+          data-type="standard"
+          data-size="large"
+          data-theme="outline"
+          data-text="sign_in_with"
+          data-shape="rectangular"
+          data-logo_alignment="left"
+        ></div>
       </div>
     </div>
   );
