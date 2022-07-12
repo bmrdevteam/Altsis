7e7c1105db7c93dd03fe3c353eec45e7af40feb3
seedlessapple <luminousseedlessapple@gmail.com>
2022-07-12 12:07:20 +0900

    Update Sidebar.tsx

--- frontend/src/components/layout/navbar/Sidebar.tsx
+++ frontend/src/components/layout/navbar/Sidebar.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Svg from "../../../assets/svg/Svg";
 
 import Nav, {
@@ -14,6 +15,16 @@ import Nav, {
 type Props = {};
 
 const Sidebar = (props: Props) => {
+  const [activeNavLink, setActiveNavLink] = useState<string>();
+
+  const navigate = useNavigate();
+
+  function openSublinkClicked(name: string) {
+    setActiveNavLink((prev: string | undefined) => {
+      return prev !== name ? name : "";
+    });
+  }
+
   return (
     <Nav>
       <NavLogo>
@@ -22,10 +33,14 @@ const Sidebar = (props: Props) => {
       <NavLinks>
         <Search />
         <NavLink
-          active
+          name="Dashboard"
+          handleClick={() => {
+            openSublinkClicked("Dashboard");
+          }}
+          active={activeNavLink === "Dashboard"}
           icon={<Svg type="analyze" />}
           subLink={
-            <SubLinks active>
+            <SubLinks>
               <SubLink icon={<Svg type="file" />}>학생 관리</SubLink>
               <SubLink icon={<Svg type="calender" />}>학생 선택</SubLink>
               <SubLink icon={<Svg type="calender" />}>학생 추가</SubLink>
@@ -34,11 +49,19 @@ const Sidebar = (props: Props) => {
         >
           대시보드
         </NavLink>
-        <NavLink active icon={<Svg type="calender" />}>
-          수업관리
-        </NavLink>
+        <NavLink icon={<Svg type="calender" />}>수업관리</NavLink>
         <NavLink icon={<Svg type="file" />}>수강신청</NavLink>
-        <NavLink icon={<Svg type="school" />}>설정</NavLink>
+        <NavLink
+          name="settings"
+          handleClick={() => {
+            openSublinkClicked("settings");
+            navigate("settings", { replace: true });
+          }}
+          active={activeNavLink === "settings"}
+          icon={<Svg type="school" />}
+        >
+          설정
+        </NavLink>
       </NavLinks>
       <NavProfile />
     </Nav>
