f9b57d1748f52b8dae8d2300bfc445a726d07d5d
seedlessapple <luminousseedlessapple@gmail.com>
2022-07-12 17:38:24 +0900

    Update index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./style/commons.scss";
+import "./style/global.scss";
 import "./style/variables.scss"
 import "./style/fonts.scss"
 import RouterPage from "./RouterPage";
