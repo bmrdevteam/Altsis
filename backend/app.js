be46ce03bfafa4f7493264105e51a43114f1fea2
jessie129j@gmail.com <jessie129j@gmail.com>
2022-07-13 15:16:31 +0900

    solve cors problem

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,10 @@ mongoose.connect(config["url"])
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors())
+app.use(cors({
+  origin: 'http://localhost:3001',
+    credentials: true
+}))
 
 app.use(
   session({
