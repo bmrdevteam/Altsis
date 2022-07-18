dbc45500e3bffa8466fbd8b0d0269d9ca1bc6e88
jessie129j@gmail.com <jessie129j@gmail.com>
2022-07-18 17:58:46 +0900

    add User CRUD

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -13,3 +13,46 @@ exports.isLoggedIn = (req, res, next) => {
         res.status(403).send({message:"You are already logged in."});
     }
  };
+
+ exports.isOwner = (req, res, next) => {
+   if (req.isAuthenticated()) {
+      if(req.session.passport.user.auth=='owner'){
+         next();
+      }
+      else{
+         res.status(401).send({message:"You are not authorized."});
+      }
+   } else {
+      res.status(403).send({message:"You are not logged in."});
+   }
+};
+
+exports.isAdmin = (req, res, next) => {
+   if (req.isAuthenticated()) {
+      if(req.session.passport.user.auth=='admin'
+      &&req.session.passport.user.academy==req.body.academy){
+         next();
+      }
+      else{
+         res.status(401).send({message:"You are not authorized."});
+      }
+   } else {
+      res.status(403).send({message:"You are not logged in."});
+   }
+};
+
+exports.authorize = (req, res, next) => {
+   if (req.isAuthenticated()) {
+      if(req.session.passport.user.auth=='owner'&&(req.body.auth=='admin'||req.body.auth=='member')){
+         next();
+      }
+      else if(req.session.passport.user.auth=='admin'&&req.body.auth=='member'){
+         next();
+      }
+      else{
+         res.status(401).send({message:"You are not authorized."});
+      }
+   } else {
+      res.status(403).send({message:"You are not logged in."});
+   }
+};
