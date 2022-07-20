c2f0068db73230b2b851f35b5718700027bf04af
seedlessapple <luminousseedlessapple@gmail.com>
2022-07-20 13:35:35 +0900

    Update Svg.tsx

--- frontend/src/assets/svg/Svg.tsx
+++ frontend/src/assets/svg/Svg.tsx
@@ -1,46 +1,12 @@
-type Props = { type: string; width?: string; height?: string };
+import { svgData } from "./svg.data";
+
+type Props = {
+  type: any;
+  width?: string;
+  height?: string;
+};
 
 const Svg = ({ type, width, height }: Props) => {
-  const result = {
-    file: (
-      <>
-        <path d="M19.903 8.586a.997.997 0 0 0-.196-.293l-6-6a.997.997 0 0 0-.293-.196c-.03-.014-.062-.022-.094-.033a.991.991 0 0 0-.259-.051C13.04 2.011 13.021 2 13 2H6c-1.103 0-2 .897-2 2v16c0 1.103.897 2 2 2h12c1.103 0 2-.897 2-2V9c0-.021-.011-.04-.013-.062a.952.952 0 0 0-.051-.259c-.01-.032-.019-.063-.033-.093zM16.586 8H14V5.414L16.586 8zM6 20V4h6v5a1 1 0 0 0 1 1h5l.002 10H6z"></path>
-        <path d="M8 12h8v2H8zm0 4h8v2H8zm0-8h2v2H8z"></path>
-      </>
-    ),
-    school: (
-      <>
-        <path d="M19 2H9c-1.103 0-2 .897-2 2v6H5c-1.103 0-2 .897-2 2v9a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V4c0-1.103-.897-2-2-2zM5 12h6v8H5v-8zm14 8h-6v-8c0-1.103-.897-2-2-2H9V4h10v16z"></path>
-        <path d="M11 6h2v2h-2zm4 0h2v2h-2zm0 4.031h2V12h-2zM15 14h2v2h-2zm-8 .001h2v2H7z"></path>
-      </>
-    ),
-    calender: (
-      <>
-        <path d="M7 11h2v2H7zm0 4h2v2H7zm4-4h2v2h-2zm0 4h2v2h-2zm4-4h2v2h-2zm0 4h2v2h-2z"></path>
-        <path d="M5 22h14c1.103 0 2-.897 2-2V6c0-1.103-.897-2-2-2h-2V2h-2v2H9V2H7v2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2zM19 8l.001 12H5V8h14z"></path>
-      </>
-    ),
-    analyze: (
-      <>
-        <path d="M3 5v14c0 1.103.897 2 2 2h14c1.103 0 2-.897 2-2V5c0-1.103-.897-2-2-2H5c-1.103 0-2 .897-2 2zm16.001 14H5V5h14l.001 14z"></path>
-        <path d="M11 7h2v10h-2zm4 3h2v7h-2zm-8 2h2v5H7z"></path>
-      </>
-    ),
-    search: (
-      <path d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z"></path>
-    ),
-    profile: (
-      <path d="M12 2a5 5 0 1 0 5 5 5 5 0 0 0-5-5zm0 8a3 3 0 1 1 3-3 3 3 0 0 1-3 3zm9 11v-1a7 7 0 0 0-7-7h-4a7 7 0 0 0-7 7v1h2v-1a5 5 0 0 1 5-5h4a5 5 0 0 1 5 5v1z"></path>
-    ),
-    logout: (
-      <>
-        <path d="M16 13v-2H7V8l-5 4 5 4v-3z"></path>
-        <path d="M20 3h-9c-1.103 0-2 .897-2 2v4h2V5h9v14h-9v-4H9v4c0 1.103.897 2 2 2h9c1.103 0 2-.897 2-2V5c0-1.103-.897-2-2-2z"></path>
-      </>
-    ),
-    menu: <path d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z"></path>,
-  };
-
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -48,7 +14,7 @@ const Svg = ({ type, width, height }: Props) => {
       height={height ?? "16"}
       viewBox="0 0 24 24"
     >
-      {result[type as keyof typeof result]}
+      {svgData[type as keyof typeof svgData]}
     </svg>
   );
 };
