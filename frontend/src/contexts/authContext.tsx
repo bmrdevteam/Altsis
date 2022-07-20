b94be216332a3aad02d860675ac0ea02edbb4944
seedlessapple <luminousseedlessapple@gmail.com>
2022-07-20 13:34:57 +0900

    Update authContext.tsx

--- frontend/src/contexts/authContext.tsx
+++ frontend/src/contexts/authContext.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { createContext, useContext, useState, useEffect } from "react";
+
 const AuthContext = createContext<any>(null);
 
 export function useAuth() {
@@ -10,18 +11,37 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [currentUser, setCurrentUser] = useState<any>(null);
   const [loading, setLoading] = useState<boolean>(true);
 
+  async function getLoggedInUser() {
+    axios
+      .get(`${process.env.REACT_APP_SERVER_URL}/api/user/info`, {
+        withCredentials: true,
+      })
+      .then((res) => {
+        setCurrentUser(res.data._user ?? null);
+      })
+      .catch((error) => {
+        error.response.status === 403 && setCurrentUser(null);
+        
+      });
+  }
+
   useEffect(() => {
-    // axios.post
-    return setLoading(false)
-  }, []);
+    return () => {
+      loading &&
+        getLoggedInUser().then(() => {
+          setLoading(false);
+        });
+    };
+  }, [loading]);
 
   const value = {
     setCurrentUser,
     currentUser,
+    loading,
   };
   return (
     <AuthContext.Provider value={value}>
-      {children}
+      {!loading && children}
     </AuthContext.Provider>
   );
 };
