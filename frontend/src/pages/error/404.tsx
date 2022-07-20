4781e59657ab799545a18fa4fa9d5f6de3c418f5
seedlessapple <luminousseedlessapple@gmail.com>
2022-07-20 13:34:28 +0900

    Update 404.tsx

--- frontend/src/pages/error/404.tsx
+++ frontend/src/pages/error/404.tsx
@@ -1,13 +1,35 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 type Props = {};
 
 const Http404 = (props: Props) => {
+  const navigate = useNavigate();
   return (
-    <>
-      <div>Http 404 cannot find page</div>
-      <div>return to home page</div>
-    </>
+    <div
+      className="page_container"
+      style={{
+        flex: "1 1 0",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        flexDirection: "column",
+        fontSize: "16px",
+      }}
+    >
+      <strong>404</strong>
+      <div>cannot find page </div>
+      <div
+        style={{ cursor: "pointer" }}
+        onClick={() => {
+          navigate("/", { replace: true });
+        }}
+      >
+        return to
+        <strong> home page</strong>
+      </div>
+    </div>
   );
 };
 
