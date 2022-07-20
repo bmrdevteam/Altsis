f196fb14eb0272e33f1ba56c1dae574630bfa872
seedlessapple <luminousseedlessapple@gmail.com>
2022-07-20 13:34:37 +0900

    Update useGoogleLogin.tsx

--- frontend/src/hooks/useGoogleLogin.tsx
+++ frontend/src/hooks/useGoogleLogin.tsx
@@ -3,56 +3,70 @@ import { useEffect, useState } from "react";
 import { useAuth } from "../contexts/authContext";
 
 export default function useGoogleLogin() {
-  const [loadingScript, setLoadingScript] = useState<boolean>();
-  const { setCurrentUser } = useAuth();
+  const [loadingScript, setLoadingScript] = useState<boolean>(true);
+  const { currentUser } = useAuth();
 
-  
   useEffect(() => {
     const getScriptTag = () => {
+      document.getElementById("googleOauthScript")?.remove();
+
       let script: HTMLScriptElement = document.createElement("script");
+
       script.src = "https://accounts.google.com/gsi/client";
       script.async = true;
       script.defer = true;
       script.id = "googleOauthScript";
-      document.body.appendChild(script);
-      setLoadingScript(false);
-    };
-    document.getElementById("googleOauthScript") === null && getScriptTag();
-  }, []);
 
+      document.body.appendChild(script);
+    };
+    getScriptTag();
+
+    setLoadingScript(false);
+  }, []);
 
   window.handleGoogleLogin = function (response: any) {
     axios
-      .post(`${process.env.REACT_APP_SERVER_URL}/api/user/google/auth`, {
-        credential: response.credential,
-      })
+      .post(
+        `${process.env.REACT_APP_SERVER_URL}/api/user/google/auth`,
+        {
+          credential: response.credential,
+        },
+        { withCredentials: true }
+      )
       .then((res) => {
-        setCurrentUser(res.data.exUser);
+        console.log(res);
+
+        //save to token
       });
   };
-
 
   return loadingScript;
 }
 export const GoogleLoginBtn = () => {
+  const { currentUser } = useAuth();
+  const status = useGoogleLogin();
   return (
     <>
-      <div
-        id="g_id_onload"
-        data-client_id={process.env.REACT_APP_GOOGLE_CLIENT_ID}
-        data-auto_prompt={true}
-        data-callback="handleGoogleLogin"
-      ></div>
-      <div
-        style={{ display: "flex", justifyContent: "center" }}
-        className="g_id_signin"
-        data-type="standard"
-        data-size="large"
-        data-theme="outline"
-        data-text="sign_in_with"
-        data-shape="rectangular"
-        data-logo_alignment="center"
-      ></div>
+      {!status && (
+        <>
+          <div
+            id="g_id_onload"
+            data-client_id={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+            data-auto_prompt={currentUser === null}
+            data-callback="handleGoogleLogin"
+          ></div>
+          <div
+            style={{ display: "flex", justifyContent: "center" }}
+            className="g_id_signin"
+            data-type="standard"
+            data-size="large"
+            data-theme="outline"
+            data-text="sign_in_with"
+            data-shape="rectangular"
+            data-logo_alignment="center"
+          ></div>
+        </>
+      )}
     </>
   );
 };
