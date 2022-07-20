4484d2be249232d3bb38288d4dd6d9e94bfdf98b
seedlessapple <luminousseedlessapple@gmail.com>
2022-07-20 13:34:54 +0900

    Create AcademyList.ts

---
+++ frontend/src/dummyData/AcademyList.ts
@@ -0,0 +1,14 @@
+export const AcademyList = [
+  {
+    _id: "1234j12kj3kj1hj234",
+    academyName: "별무리학교",
+    academyId: "bmr",
+    userCount: 324,
+  },
+  {
+    _id: "qweroi13241kj3h4",
+    academyName: "난초학교",
+    academyId: "qwerty",
+    userCount: 123,
+  },
+];
