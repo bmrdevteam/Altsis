4a156c9ee019bf29a288ac22f010a15edf3f3092
seedlessapple <luminousseedlessapple@gmail.com>
2022-07-25 13:44:42 +0900

    Create EditorTable.tsx

---
+++ frontend/src/components/UI/editor/items/table/EditorTable.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { editorData } from "../../../../../dummyData/editorData";
+import style from "./editorTable.module.scss";
+import TableCell from "./TableCell";
+
+type Props = {};
+
+const Table = (props: Props) => {
+  const [tableData, setTableData] = useState(editorData.data[1]);
+  console.log(tableData);
+
+  return (
+    <div className={style.table_container} style={{ gridTemplateColumns: "" }}>
+      <div className={style.grid}>
+        {tableData.data?.map((value, index) => {
+          return (
+            <TableCell
+              key={index}
+              col={value.col}
+              row={value.row}
+              colSpan={value.colspan}
+              rowSpan={value.rowspan}
+            >
+              {value.content}
+            </TableCell>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Table;
