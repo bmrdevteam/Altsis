ef2dce19ef45ba4313baf4c52beec2e8c8848373
seedlessapple <luminousseedlessapple@gmail.com>
2022-07-25 13:44:58 +0900

    Create editorData.ts

---
+++ frontend/src/dummyData/editorData.ts
@@ -0,0 +1,54 @@
+export const editorData = {
+  _id: "1219908fd980193udf",
+  timestamps: "2022-06-18 10:00:00",
+  userId: "seedlessapple",
+  userName: "dltpcks",
+  schoolId: "bmrhs",
+  data: [
+    {
+      type: "text",
+      content: "",
+    },
+    {
+      type: "table",
+      header: {
+        rows: 5,
+        cols: 5,
+      },
+      data: [
+        {
+          cellType: "header",
+          row: 1,
+          col: 1,
+          colspan: 1,
+          rowspan: 1,
+          content: "hello this is a test",
+        },
+        {
+          cellType: "text",
+          row: 1,
+          col: 2,
+          colspan: 1,
+          rowspan: 1,
+          content: "hello this is a test2",
+        },
+        {
+          cellType: "text",
+          row: 1,
+          col: 3,
+          colspan: 1,
+          rowspan: 1,
+          content: "hello this is a test2",
+        },
+        {
+          cellType: "text",
+          row: 2,
+          col: 3,
+          colspan: 3,
+          rowspan: 3,
+          content: "hello this is a test2",
+        },
+      ],
+    },
+  ],
+};
