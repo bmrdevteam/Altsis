a2d208bde666743de4c87b5c63332d980f4f95a8
seedlessapple <luminousseedlessapple@gmail.com>
2022-07-27 01:50:54 +0900

    Create dataTypes.ts

---
+++ frontend/src/components/editorV2/types/dataTypes.ts
@@ -0,0 +1,22 @@
+export interface IBlock {
+  id: string;
+  type: "heading" | "paragraph" | "table" | "divider";
+}
+export interface IParagraphBlock extends IBlock {
+  data: {
+    text: string;
+  };
+}
+export interface IHeadingBlock extends IBlock {
+  data: {
+    text: string;
+  };
+}
+export interface IMenuItem {
+  icon: JSX.Element;
+  text: string;
+}
+
+export interface IAppearanceMenuItem extends Omit<IMenuItem, "text"> {
+
+}
