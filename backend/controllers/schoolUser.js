1fc1ede4cef595aed5edf3ab2ce4b418513e3ef1
jessie129j@gmail.com <jessie129j@gmail.com>
2022-08-03 15:10:52 +0900

    add schoolUser APIs

--- backend/controllers/schoolUser.js
+++ backend/controllers/schoolUser.js
@@ -1,35 +1,23 @@
 const SchoolUser = require("../models/SchoolUser");
-const { checkSchema, validationResult } = require("express-validator");
 
-exports.read = async (req, res) => {
+exports.register= async (req,res) => {
     try {
-        const user = await SchoolUser(req.academy).find(req.query);
-        return res.status(200).send({user})
-    }
-    catch (err) {
-        if (err) return res.status(500).send({ err: err.message });
-    }
-}
-
-exports.update = async (req, res) => {
-    try {
-        const user=req.body.user[0];
-        if(user.password){
-            user.password=await generateHash(user.password)
+        const schoolUser = await SchoolUser(req.user.dbName).findById(req.body._id);
+        const idx = schoolUser["registrations"].findIndex(obj => obj.year.year === req.body.year.year);
+        if(idx==-1){
+            schoolUser["registrations"].push({
+                year: req.body.year,
+                terms:[
+                    req.body.term
+                ]
+            })
         }
-        const updatedUser = await SchoolUser(req.academy).findByIdAndUpdate(user._id, user,{ returnDocument: 'after' });
-        return res.status(200).send({ user: updatedUser })
-    }
-    catch (err) {
-        if (err) return res.status(500).send({ err: err.message });
-    }
-}
-
-
-exports.delete = async (req, res) => {
-    try {
-        const doc = await SchoolUser(req.academy).findByIdAndDelete(req.query._id);
-        return res.status(200).send({success:(!!doc)})
+        else{
+            schoolUser["registrations"][idx]['terms'].push(req.body.term);
+            schoolUser.markModified('registrations');
+        } 
+        await schoolUser.save();
+        return res.status(200).send({schoolUser})
     }
     catch (err) {
         return res.status(500).send({ err: err.message });
@@ -37,3 +25,104 @@ exports.delete = async (req, res) => {
 }
 
 
+exports.read=async(req,res)=>{
+    try {
+        const schoolUser = await SchoolUser(req.user.dbName).findById(req.params._id);
+        return res.status(200).send({schoolUser})
+    }
+    catch (err) {
+        return res.status(500).send({ err: err.message });
+    }
+}
+
+exports.list=async(req,res)=>{
+    try {
+        const schoolUsers = await SchoolUser(req.user.dbName).find({});
+        return res.status(200).send({schoolUsers})
+    }
+    catch (err) {
+        return res.status(500).send({ err: err.message });
+    }
+}
+
+exports.updateRegistration=async(req,res)=>{
+    try {
+        const schoolUser = await SchoolUser(req.user.dbName).findById(req.body._id);
+        const yearIdx = schoolUser["registrations"].findIndex(obj => obj.year.year === req.body.year.year);
+        if(yearIdx==-1){
+            return res.status(404).send({message:"not existing..."});
+            
+        }
+        else{
+            schoolUser["registrations"][yearIdx]=req.body.year;
+            const termIdx=schoolUser["registrations"][yearIdx]['terms'].findIndex(obj => obj.term.term === req.body.term.term);
+            schoolUser["registrations"][yearIdx]['terms'][termIdx]=req.body.term;
+            schoolUser.markModified('registrations');
+        } 
+        await schoolUser.save();
+        return res.status(200).send({schoolUser})
+      }
+      catch (err) {
+          if (err) return res.status(500).send({ err: err.message });
+      }
+  }
+
+exports.update=async(req,res)=>{
+  try {
+        const schoolUser=await SchoolUser(req.user.dbName).findById(req.params._id);
+        const fields=['role','photo','info']
+        if(fields.includes(req.params.field)){
+            schoolUser[req.params.field]=req.body.new;
+        }
+        else if(req.params.field=='registration'){
+            console.log('...')
+            const yearIdx = schoolUser["registrations"].findIndex(obj => obj.year.year === req.body.year.year);
+            if(yearIdx==-1){
+                return res.status(404).send({message:"not existing..."});
+            }
+            
+            schoolUser["registrations"][yearIdx]['year']=req.body.year;
+            console.log(schoolUser["registrations"][yearIdx]) 
+            const termIdx=schoolUser["registrations"][yearIdx]['terms'].findIndex(obj => obj.term === req.body.term.term);
+            if(termIdx==-1){
+                return res.status(404).send({message:"not existing..."});
+            }
+            schoolUser["registrations"][yearIdx]['terms'][termIdx]=req.body.term;
+            schoolUser.markModified('registrations');
+        }
+        else{
+            return res.status(400).send({message:`field '${req.params.field}' does not exist or cannot be updated`});
+        }
+        await schoolUser.save();
+        return res.status(200).send({schoolUser})
+    }
+    catch (err) {
+        if (err) return res.status(500).send({ err: err.message });
+    }
+}
+
+exports.deleteRegistration=async(req,res)=>{
+    try {
+        const schoolUser=await SchoolUser(req.user.dbName).findById(req.params._id);
+        const yearIdx = schoolUser["registrations"].findIndex(obj => obj.year.year === req.query.year);
+        if(yearIdx==-1){
+            return res.status(404).send({message:"not existing year"});
+        }
+        const termIdx=schoolUser["registrations"][yearIdx]['terms'].findIndex(obj => obj.term === req.query.term);
+        if(termIdx==-1){
+            return res.status(404).send({message:"not existing term"});
+        }
+        else if(termIdx==0){
+            schoolUser["registrations"].splice(yearIdx, 1);
+        }
+        else{
+            schoolUser["registrations"][yearIdx]['terms'].splice(termIdx, 1);
+        }
+        schoolUser.markModified('registrations');
+        await schoolUser.save();
+        return res.status(200).send({schoolUser})
+      }
+      catch (err) {
+          if (err) return res.status(500).send({ err: err.message });
+      }
+  }
