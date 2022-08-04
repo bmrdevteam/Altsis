790754b5d144f48d1f46199df394e4f2f8a77904
jessie129j@gmail.com <jessie129j@gmail.com>
2022-08-04 09:29:32 +0900

    update syllabusAPI

--- backend/routes/syllabuses.js
+++ backend/routes/syllabuses.js
@@ -8,8 +8,11 @@ const {isAdmin, isLoggedIn}=require('../middleware/auth')
 //=================================
 
 router.post('/',isLoggedIn,syllabus.create);
+
 router.get('/list',isLoggedIn,syllabus.list);
 router.get('/:_id',isLoggedIn,syllabus.read);
+router.get('/:_id/students',isLoggedIn,syllabus.students);
+
 router.put('/:_id',isLoggedIn,syllabus.update)
 router.delete('/:_id',isLoggedIn,syllabus.delete);
 
