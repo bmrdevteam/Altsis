333e7f5e087466982f33cc2bcdbaad7b9e19d069
seedlessapple <luminousseedlessapple@gmail.com>
2022-08-07 15:51:47 +0900

    Update Test.tsx

--- frontend/src/pages/dev/Test.tsx
+++ frontend/src/pages/dev/Test.tsx
@@ -5,10 +5,18 @@ import { TableDummy } from "../../dummyData/TableDummy.data";
 type Props = {};
 
 const Test = (props: Props) => {
-  
   return (
     <div>
-      <Table data={TableDummy} header={[{text:"id",key:"id",type:"index"}]} />
+      <Table
+        data={TableDummy}
+        header={[
+          { text: "", key: "", type: "checkbox" ,width:"48px",align:"center"},
+          { text: "id", key: "id", type: "index" ,width:"48px",align:"center"},
+          { text: "the firsta", key: "boolean", type: "time" ,align:"right"},
+          { text: "namefield", key: "userId", type: "string" },
+          { text: "email?", key: "s", type: "string" },
+        ]}
+      />
     </div>
   );
 };
