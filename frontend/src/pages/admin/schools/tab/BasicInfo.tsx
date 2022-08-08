8dc31ca7f2d1f54c90743096dbd08f6c2ffeccc8
seedlessapple <luminousseedlessapple@gmail.com>
2022-08-08 09:04:59 +0900

    Update BasicInfo.tsx

--- frontend/src/pages/admin/schools/tab/BasicInfo.tsx
+++ frontend/src/pages/admin/schools/tab/BasicInfo.tsx
@@ -1,30 +1,23 @@
 import React from "react";
+import Autofill from "../../../../components/input/Autofill";
+import Input from "../../../../components/input/Input";
 import style from "../../../../style/pages/admin/schools/schools.module.scss";
 type Props = {};
 
 const BasicInfo = (props: Props) => {
   return (
     <div>
-      <div style={{ marginTop: "24px", display: "flex",width:"100%" }}>
-        <div style={{ display: "flex", flexDirection: "column-reverse" }}>
-          <input
-            type="text"
-            style={{ fontSize: "14px", padding: "8px", outline: "none",width:"100%" }}
-          />
-          <label style={{ fontSize: "12px", paddingBottom: "12px" }}>
-            학교명
-          </label>
-        </div>
-        <div style={{ display: "flex", flexDirection: "column-reverse" }}>
-          <input
-            type="text"
-            style={{ fontSize: "14px", padding: "8px", outline: "none" }}
-          />
-          <label style={{ fontSize: "12px", paddingBottom: "12px" }}>
-            학교명
-          </label>
-        </div>
+      <div
+        style={{
+          display: "flex",
+          width: "100%",
+          gap: "24px",
+        }}
+      >
+        <Input label="학교명" />
+        <Input label="학교ID" />
       </div>
+      <Autofill label="관리자" options={["goat@gmail.com","ebfhschool@gmail.com","asdf","ramdonId","한국어","한구어@?"]} />
     </div>
   );
 };
