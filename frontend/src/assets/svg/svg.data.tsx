b88054805732511c37e4164820a30d94bdee1274
seedlessapple <luminousseedlessapple@gmail.com>
2022-08-09 11:58:09 +0900

    Update svg.data.tsx

--- frontend/src/assets/svg/svg.data.tsx
+++ frontend/src/assets/svg/svg.data.tsx
@@ -67,4 +67,5 @@ export const svgData = {
   checkboxMinus: (
     <path d="M17 5H7a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V7a2 2 0 0 0-2-2zm-1 8H8v-2h8z"></path>
   ),
+  caretDown: <path d="m11.998 17 7-8h-14z"></path>,
 };
