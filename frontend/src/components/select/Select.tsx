2cdf04b8e08cec6cf168b5988a63396acc672139
seedlessapple <luminousseedlessapple@gmail.com>
2022-08-09 13:28:07 +0900

    Update Select.tsx

--- frontend/src/components/select/Select.tsx
+++ frontend/src/components/select/Select.tsx
@@ -1,15 +1,35 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import Svg from "../../assets/svg/Svg";
 import style from "./select.module.scss";
 type Props = {
   options: {
     text: string;
     value: string | number;
   }[];
+  style?: {
+    minWidth?: string;
+    width?: string;
+  };
+  ref?: any;
 };
 
 const Select = (props: Props) => {
   const [selected, setSelected] = useState<number>(0);
   const [edit, setEdit] = useState<boolean>(false);
+  const selectRef = useRef<HTMLDivElement>(null);
+
+  function handleMousedown(e: MouseEvent) {
+    if (selectRef.current && !selectRef.current.contains(e.target as Node)) {
+      console.log("You clicked outside of me!");
+    }
+  }
+  useEffect(() => {
+    document.addEventListener("mousedown", handleMousedown);
+
+    return () => {
+      document.removeEventListener("mousedown", handleMousedown);
+    };
+  }, []);
 
   const Options = () => {
     return (
@@ -34,14 +54,23 @@ const Select = (props: Props) => {
   };
 
   return (
-    <div className={style.select}>
+    <div
+      ref={selectRef}
+      className={style.select}
+      style={{ width: props.style?.width }}
+    >
       <div
+        ref={props.ref}
         className={style.selected}
+        style={{ minWidth: props.style?.minWidth, width: props.style?.width }}
         onClick={() => {
-          setEdit(true);
+          setEdit((prev) => !prev);
         }}
       >
-        {props.options[selected].text}
+        <span className={style.text}>{props.options[selected].text}</span>
+        <span className={style.icon}>
+          <Svg type={"caretDown"} />
+        </span>
       </div>
       {edit && <Options />}
     </div>
