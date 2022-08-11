c6ca4f6dc6273c7c3137082380fe00a44457c23d
seedlessapple <luminousseedlessapple@gmail.com>
2022-08-11 11:27:22 +0900

    Update Input.tsx

--- frontend/src/components/input/Input.tsx
+++ frontend/src/components/input/Input.tsx
@@ -4,20 +4,27 @@ type Props = {
   defaultValue?: string;
   placeholder?: string;
   label?: string;
+  required?: boolean;
+  disabled?: boolean;
+  onChange?: any;
 };
 
 const Input = (props: Props) => {
   return (
     <div className={style.input_container}>
+      <label className={style.label}>
+        {props.label}
+        {props.required && <span className={style.required}>*</span>}
+      </label>
       <input
         type="text"
         className={style.input}
         defaultValue={props.defaultValue}
         placeholder={props.placeholder}
+        onChange={props.onChange}
+        required={props.required}
+        disabled={props.disabled}
       />
-      <label style={{ fontSize: "12px", paddingBottom: "6px" }}>
-        {props.label}
-      </label>
     </div>
   );
 };
