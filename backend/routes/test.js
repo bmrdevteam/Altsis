e338de311c9654c3daf37d944acbefd479f8ff05
jessie129j@gmail.com <jessie129j@gmail.com>
2022-08-12 15:08:58 +0900

    check time and classroom when updating syllabus and enrolling

--- backend/routes/test.js
+++ backend/routes/test.js
@@ -3,7 +3,7 @@ const School = require('../models/School');
 const Syllabus=require('../models/Syllabus');
 const router = express.Router();
 const {classroomsTable}=require('../utils/util');
-
+const _ = require('lodash');
 // const {data,add}=require('../databases/connection')   
 // router.post('/test2', (req, res) => {
 //     add({key:req.body.key,val:req.body.val});
@@ -11,6 +11,42 @@ const {classroomsTable}=require('../utils/util');
 //         data
 //     })
 // })
+
+router.get('/lodash',(req,res)=>{
+    const arr11_1=[1,1];
+    const arr11_2=[1,1];
+    const arr12=[1,2];
+
+    const res1=_.isEqual(arr11_1,arr11_2);
+    const res2=_.isEqual(arr11_1,arr12);
+
+    const obj1={a:1,b:1};
+    const obj2={a:1,b:1};
+    const obj3={a:1,b:2};
+
+    const res3=_.isEqual(obj1,obj2);
+    const res4=_.isEqual(obj1,obj3);
+
+    const objArr1=[{a:1,b:1},{a:1,b:1}];
+    const objArr2=[{a:1,b:1},{a:1,b:1}];
+    const objArr3=[{a:1,b:1},{a:1,b:2}];
+
+    const res5=_.isEqual(objArr1,objArr2);
+    const res6=_.isEqual(objArr1,objArr3);
+
+    const test1= [ { label: '월5', day: '월', start: '9:00', end: '9:50' },{ label: '월2', day: '월', start: '10:00', end: '10:50' }];
+    const test2= [ { label: '월5', day: '월', start: '9:00', end: '9:50' },{ label: '월2', day: '월', start: '10:00', end: '10:55' }];
+
+    let res7=[];
+    for(let i=0;i<test1.length;i++){
+        res7.push(
+            [test1[i],test2[i],_.isEqual(test1[i],test2[i])]
+        )
+    }
+    const res8=_.difference(test1,test2);
+
+    return res.status(200).send({res1,res2,res3,res4,res5,res6,res7,res8});
+})
 
 router.get('/classrooms', async (req, res) => {
 
