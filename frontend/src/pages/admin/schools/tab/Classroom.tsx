65db1200507a7be59ef21a3df3f1aefaad83de76
seedlessapple <luminousseedlessapple@gmail.com>
2022-08-17 15:21:47 +0900

    Update Classroom.tsx

--- frontend/src/pages/admin/schools/tab/Classroom.tsx
+++ frontend/src/pages/admin/schools/tab/Classroom.tsx
@@ -1,23 +1,48 @@
-import React from "react";
-import NavigationLinks from "../../../../components/navigationLinks/NavigationLinks";
+import { useState } from "react";
+import Svg from "../../../../assets/svg/Svg";
+import Button from "../../../../components/button/Button";
+import Input from "../../../../components/input/Input";
+import Popup from "../../../../components/popup/Popup";
 import Table from "../../../../components/table/Table";
-import style from "../../../../style/pages/academy/schools/schools.module.scss";
-type Props = {};
+import useDatabase from "../../../../hooks/useDatabase";
+import style from "../../../../style/pages/admin/schools/schools.module.scss";
+type Props = {
+  school: any;
+};
 
 const Classroom = (props: Props) => {
+  const [addClassroomPopupActive, setAddClassRoomPopupActive] =
+    useState<boolean>(false);
+  const [inputClassroomName, setInputClassroomName] = useState<string>();
+  const database = useDatabase();
+
+  console.log(`${props.school._id}/classrooms`);
+  
+  // addclassroom function
+  async function addClassroom() {
+    await database.C({
+      location: `${props.school._id}/classrooms`,
+      data: { new: inputClassroomName },
+    });
+  }
+
+  //
   return (
     <div style={{ marginTop: "24px" }}>
+      <Button
+        type={"ghost"}
+        borderRadius={"4px"}
+        height={"32px"}
+        onClick={() => {
+          setAddClassRoomPopupActive(true);
+        }}
+      >
+        + 새로운 강의실 추가
+      </Button>
+      <div style={{ marginTop: "24px" }}></div>
+
       <Table
-        data={[
-          {
-            id: "j23htjnasdbd34",
-            classroom: "202호",
-          },
-          {
-            id: "2135ljh312bdas",
-            classroom: "102호",
-          },
-        ]}
+        data={props.school?.classrooms}
         header={[
           {
             text: "ID",
@@ -28,21 +53,65 @@ const Classroom = (props: Props) => {
           },
           {
             text: "강의실",
-            key: "classroom",
-            type: "string",
+            key: "",
+            type: "arrText",
           },
-
           {
             text: "자세히",
             key: "schoolId",
-            type: "link",
-            link: "/academy/schools",
+            type: "button",
+            onClick: () => {},
+
             width: "80px",
             align: "center",
           },
         ]}
         style={{ backgroundColor: "#fff" }}
       />
+      {addClassroomPopupActive && (
+        <Popup setState={setAddClassRoomPopupActive}>
+          <div className={style.popup_container}>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "flex-start",
+              }}
+            >
+              <div className={style.title}>강의실 추가</div>
+              <div
+                className={style.x}
+                onClick={() => {
+                  setAddClassRoomPopupActive(false);
+                }}
+              >
+                <Svg type="x" width="24px" height="24px" />
+              </div>
+            </div>
+            <div style={{ marginTop: "24px" }}>
+              <Input
+                label="강의실 이름"
+                required
+                onChange={(e: any) => {
+                  setInputClassroomName(e.target.value);
+                }}
+              />
+            </div>
+            <div style={{ marginTop: "24px" }}>
+              <Button
+                onClick={(e: any) => {
+                  e.preventDefault();
+                  addClassroom().then((res) => {
+                    console.log(res);
+                  });
+                }}
+              >
+                추가
+              </Button>
+            </div>
+          </div>
+        </Popup>
+      )}
     </div>
   );
 };
