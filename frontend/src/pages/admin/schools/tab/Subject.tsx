74ccea48753572622c6ac22b26296bdcdbe23257
seedlessapple <luminousseedlessapple@gmail.com>
2022-08-17 15:21:52 +0900

    Update Subject.tsx

--- frontend/src/pages/admin/schools/tab/Subject.tsx
+++ frontend/src/pages/admin/schools/tab/Subject.tsx
@@ -1,11 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import Button from "../../../../components/button/Button";
+import Table from "../../../../components/table/Table";
 
-type Props = {}
+type Props = {
+  school: any;
+};
 
 const Subject = (props: Props) => {
-  return (
-    <div>Subject</div>
-  )
-}
+  const [subjects, setSubjects] = useState([]);
 
-export default Subject
+  useEffect(() => {
+    props.school.data.map(() => {});
+
+    return () => {};
+  }, []);
+
+  return (
+    <div style={{ marginTop: "24px" }}>
+      <Button
+        type={"ghost"}
+        borderRadius={"4px"}
+        height={"32px"}
+        onClick={() => {}}
+      >
+        + 새로운 강의실 추가
+      </Button>
+      <div style={{ marginTop: "24px" }}></div>
+
+      <Table
+        data={props.school?.subject}
+        header={[
+          {
+            text: "ID",
+            key: "",
+            type: "index",
+            width: "48px",
+            align: "center",
+          },
+          {
+            text: "교과목",
+            key: "",
+            type: "arrText",
+          },
+          {
+            text: "자세히",
+            key: "schoolId",
+            type: "link",
+            link: "/academy/schools",
+            width: "80px",
+            align: "center",
+          },
+        ]}
+        style={{ backgroundColor: "#fff" }}
+      />
+    </div>
+  );
+};
+
+export default Subject;
