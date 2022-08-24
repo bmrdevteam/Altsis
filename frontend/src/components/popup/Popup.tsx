3d225d4869462392e18ad2d7a08049d006b1071c
seedlessapple <luminousseedlessapple@gmail.com>
2022-08-24 14:16:17 +0900

    Update Popup.tsx

--- frontend/src/components/popup/Popup.tsx
+++ frontend/src/components/popup/Popup.tsx
@@ -1,9 +1,13 @@
+import Svg from "../../assets/svg/Svg";
+import style from "./popup.module.scss";
 const Popup = ({
   children,
   setState,
+  title,
 }: {
   children: JSX.Element | JSX.Element[];
   setState: any;
+  title: string;
 }) => {
   return (
     <div
@@ -13,9 +17,10 @@ const Popup = ({
         left: 0,
         width: "100vw",
         height: "100vh",
-        display:"flex",
-        justifyContent:"center",
-        alignItems:"center"
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        zIndex: 2024,
       }}
     >
       <div
@@ -24,7 +29,7 @@ const Popup = ({
           height: "100vh",
           zIndex: 2025,
           backgroundColor: "rgba(0,0,0,0.3)",
-          backdropFilter:"blur(1px)"
+          backdropFilter: "blur(1px)",
         }}
         onClick={() => {
           setState(false);
@@ -37,7 +42,26 @@ const Popup = ({
           backgroundColor: "#fff",
         }}
       >
-        {children}
+        <div className={style.popup_container}>
+          <div
+            style={{
+              display: "flex",
+
+              alignItems: "flex-start",
+            }}
+          >
+            <div className={style.title}>{title}</div>
+            <div
+              className={style.x}
+              onClick={() => {
+                setState(false);
+              }}
+            >
+              <Svg type="x" width="24px" height="24px" />
+            </div>
+          </div>
+          {children}
+        </div>
       </div>
     </div>
   );
