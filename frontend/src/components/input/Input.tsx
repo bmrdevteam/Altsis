8388983c9fb580c9650e71eb54a445c8514d313c
seedlessapple <luminousseedlessapple@gmail.com>
2022-08-24 14:16:12 +0900

    Update Input.tsx

--- frontend/src/components/input/Input.tsx
+++ frontend/src/components/input/Input.tsx
@@ -7,15 +7,18 @@ type Props = {
   required?: boolean;
   disabled?: boolean;
   onChange?: any;
+  onKeyDown?: any;
 };
 
 const Input = (props: Props) => {
   return (
     <div className={style.input_container}>
-      <label className={style.label}>
-        {props.label}
-        {props.required && <span className={style.required}>*</span>}
-      </label>
+      {props.label && (
+        <label className={style.label}>
+          {props.label}
+          {props.required && <span className={style.required}>*</span>}
+        </label>
+      )}
       <input
         type="text"
         className={style.input}
@@ -24,6 +27,7 @@ const Input = (props: Props) => {
         onChange={props.onChange}
         required={props.required}
         disabled={props.disabled}
+        onKeyDown={props.onKeyDown}
       />
     </div>
   );
