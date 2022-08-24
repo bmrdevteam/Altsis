a1095b70253640306ad7834f8366a04d302a66d2
jessie129j@gmail.com <jessie129j@gmail.com>
2022-08-24 04:19:26 +0000

    add backend Dockerfile

--- backend/databases/root.js
+++ backend/databases/root.js
@@ -1,4 +1,4 @@
 const mongoose=require('mongoose')
 const config=require('../config/config')
 
-module.exports =mongoose.createConnection(config["newUrl"]("root"))
+module.exports =mongoose.createConnection(config["url"]("root"))
