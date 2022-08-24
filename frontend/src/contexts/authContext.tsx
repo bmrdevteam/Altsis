024f43a1fa33b63bdc9bd2ae8541c659adf67a81
seedlessapple <luminousseedlessapple@gmail.com>
2022-08-24 14:17:00 +0900

    Update authContext.tsx

--- frontend/src/contexts/authContext.tsx
+++ frontend/src/contexts/authContext.tsx
@@ -9,32 +9,38 @@ export function useAuth() {
 }
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  const database = useDatabase()
+  const database = useDatabase();
   const [currentUser, setCurrentUser] = useState<any>(null);
+  const [currentSchool, setCurrentSchool] = useState<any>(null);
   const [loading, setLoading] = useState<boolean>(true);
 
   async function getLoggedInUser() {
-   const {user:res} = await database.R({location:"users"})
-   setCurrentUser(res)
-   console.log(res);
-   return res
+    const { user: res } = await database.R({ location: "users" });
+    setCurrentUser(res);
+    return res;
   }
 
   useEffect(() => {
-    return () => {
-      loading &&
-        getLoggedInUser().then(() => {
+    loading &&
+      getLoggedInUser()
+        .then(() => {
           setLoading(false);
-        }).catch((err)=>{
-          setLoading(false);
+          // setCurrentSchool();
         })
-    };
+        .catch((err) => {
+          setLoading(false);
+        });
+
+    return () => {};
   }, [loading]);
 
   const value = {
     setCurrentUser,
     currentUser,
     loading,
+    setLoading,
+    currentSchool,
+    setCurrentSchool,
   };
   return (
     <AuthContext.Provider value={value}>
