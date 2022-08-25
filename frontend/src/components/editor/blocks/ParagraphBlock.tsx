14b9a07b746ba16eaad955ff87c2e832a210c67e
seedlessapple <luminousseedlessapple@gmail.com>
2022-08-25 13:24:20 +0900

    Update ParagraphBlock.tsx

--- frontend/src/components/editor/blocks/ParagraphBlock.tsx
+++ frontend/src/components/editor/blocks/ParagraphBlock.tsx
@@ -1,17 +1,56 @@
-
+import { useEffect, useRef } from "react";
 import style from "../editor.module.scss";
 import { IParagraphBlock } from "../type";
 
+const Paragraphblock = ({
+  block,
+  editorFunctions,
+}: {
+  block: IParagraphBlock;
+  editorFunctions: any;
+}) => {
+  const divRef = useRef<HTMLDivElement>(null);
 
-const Paragraphblock = ({ block }: { block: IParagraphBlock }) => {
-  
+  useEffect(() => {}, [divRef]);
 
   return (
-    <input
+    <div
+      ref={divRef}
       className={style.paragraph}
-      defaultValue={block.data.text}  
-      onClick={()=>{
-        
+      placeholder={"입력"}
+      contentEditable
+      suppressContentEditableWarning
+      onKeyDown={(e) => {
+        if (e.key === "Enter") {
+          e.preventDefault();
+          editorFunctions.addBlock({
+            insertAfter: editorFunctions.getBlockIndex(block.id) + 1,
+          });
+
+          editorFunctions.saveBlock({
+            block: {
+              id: block.id,
+              type: block.type,
+              data: { text: e.target.innerHTML },
+            },
+          });
+        }
+        if (e.key === "Backspace" && e.target.innerText === "") {
+          editorFunctions.deleteBlock(block.id);
+        }
+      }}
+      onInput={(e: any) => {
+        editorFunctions.saveBlock({
+          block: {
+            id: block.id,
+            type: block.type,
+            data: { text: e.target.innerHTML },
+          },
+          update: false,
+        });
+      }}
+      onClick={() => {
+        console.log();
       }}
       // onSelect={(e) => {
       //   console.log(
@@ -21,8 +60,8 @@ const Paragraphblock = ({ block }: { block: IParagraphBlock }) => {
       //   );
       // }}
     >
-      {/* {block.data.text} */}
-    </input>
+      {block.data.text}
+    </div>
   );
 };
 
