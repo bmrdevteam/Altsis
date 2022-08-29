30212275b525c4917a5cca71cd839d62137cd8b8
seedlessapple <luminousseedlessapple@gmail.com>
2022-08-29 13:05:30 +0900

    server

---
+++ frontend/server.js
@@ -0,0 +1,25 @@
+const http = require("http");
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = 3030
+
+app.get("/ping", (req, res) => {
+  res.send("pong");
+});
+
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/*", (req, res) => {
+  res.set({
+    "Cache-Control": "no-cache, no-store, must-revalidate",
+    Pragma: "no-cache",
+    Date: Date.now()
+  });
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+http.createServer(app).listen(port, () => {
+  console.log(`app listening at ${port}`);
+});
