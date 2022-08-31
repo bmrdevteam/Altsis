863e16c852c5cd95d71bf3ed8687d80896366163
jessie129j@gmail.com <jessie129j@gmail.com>
2022-08-31 15:38:17 +0900

    feat: profile image upload

---
+++ backend/controllers/profile.js
@@ -0,0 +1,89 @@
+const User = require("../models/User");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+const aws = require("aws-sdk");
+const config = require("../config/config");
+const s3 = new aws.S3(config.s3);
+const bucket = "bsis-profile";
+
+const whitelist = ["image/png", "image/jpeg", "image/jpg", "image/webp"];
+
+const myMulter = multer({
+  limits: {
+    files: 1,
+    fileSize: 2 * 1024 * 1024,
+  },
+  storage: multerS3({
+    s3: s3,
+    bucket: bucket,
+    acl: "public-read",
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    key: function (req, file, cb) {
+      cb(null, `original/${req.user.academy.academyId}_${req.user._id}`);
+    },
+  }),
+  fileFilter: (req, file, cb) => {
+    if (!whitelist.includes(file.mimetype)) {
+      const err = new Error("file is not allowed");
+      err.code = "INVALID_FILE_TYPE";
+      return cb(err);
+    }
+    cb(null, true);
+  },
+});
+
+exports.upload = async (req, res) => {
+  const user = req.user;
+
+  myMulter.single("img")(req, {}, async (err) => {
+    if (err) {
+      if (err.code == "LIMIT_FILE_SIZE" || err.code == "INVALID_FILE_TYPE")
+        return res.status(409).send({ err });
+      return res.status(500).send({ err });
+    }
+
+    const originalUrl = req.file.location;
+    const url = originalUrl.replace(/\/original\//, "/thumb/");
+
+    if (user.profile) {
+      try {
+        await s3.deleteObject({
+          Bucket: bucket,
+          Key: `thumb/${user.academy.academyId}_${user._id}`,
+        });
+      } catch (err) {
+        return res.status(500).send({ err: err.message });
+      }
+    } else {
+      user.profile = url;
+      await user.save();
+    }
+
+    return res.status(200).send({ url, originalUrl });
+  });
+};
+
+exports.read = (req, res) => {
+  const url = req.user.profile || config.defaultProfile;
+  return res.status(200).send({ url });
+};
+
+exports.delete = (req, res) => {
+  const user = req.user;
+  if (!user.profile) {
+    return res.status(404).send({ message: "no profile!" });
+  }
+
+  s3.deleteObject(
+    { Bucket: bucket, Key: `original/${user.academy.academyId}_${user._id}` },
+    async (err, data) => {
+      if (err) {
+        return res.status(500).send({ err: err.message });
+      }
+      user.profile = undefined;
+      await user.save();
+
+      return res.status(200).send({ success: true });
+    }
+  );
+};
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,63 +1,72 @@
-const mongoose=require('mongoose')
-const bcrypt = require('bcrypt');
-const moment=require('moment');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const moment = require("moment");
 
-var config=require('../config/config.js')
+var config = require("../config/config.js");
 
-const {conn}=require('../databases/connection')   
+const { conn } = require("../databases/connection");
 
-const userSchema=mongoose.Schema({
-    
-    userId:{
-        type:String,
-        unique:true
+const userSchema = mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      unique: true,
     },
-    userName:String,
-    password:String,
-    auth:String,
-    email:String,
-    tel:String,
-    snsId:[
-        mongoose.Schema({
-            provider:String,
-            email:String
-        },{_id:false})
+    userName: String,
+    password: String,
+    auth: String,
+    email: String,
+    tel: String,
+    snsId: [
+      mongoose.Schema(
+        {
+          provider: String,
+          email: String,
+        },
+        { _id: false }
+      ),
     ],
-    schools:[
-        mongoose.Schema({
-            schoolId:String,
-            schoolName:String
-        },{_id:false})
-    ]
-},{ timestamps: true });
+    schools: [
+      mongoose.Schema(
+        {
+          schoolId: String,
+          schoolName: String,
+        },
+        { _id: false }
+      ),
+    ],
+    profile: String,
+  },
+  { timestamps: true }
+);
 
-userSchema.pre('save',function(next){
-    var user=this;
-    if(user.isModified('password')){ //비밀번호가 바뀔때만 암호화
-        bcrypt.genSalt(config.saltRounds,function(err,salt){
-            if(err) return next(err);
-            bcrypt.hash(user.password,salt,function(err,hash){
-                if(err) return next(err);
-                user.password=hash;
-                next();
-            })
-        })
-    }else{
-        next()
-    }
-})
+userSchema.pre("save", function (next) {
+  var user = this;
+  if (user.isModified("password")) {
+    //비밀번호가 바뀔때만 암호화
+    bcrypt.genSalt(config.saltRounds, function (err, salt) {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
 
-userSchema.methods.comparePassword=async function(plainPassword){
-    var user=this;
-    try{
-        const isMatch = await bcrypt.compare(plainPassword,user.password)
-        return isMatch
-    }
-    catch(err){
-        return err
-    }
-}
+userSchema.methods.comparePassword = async function (plainPassword) {
+  var user = this;
+  try {
+    const isMatch = await bcrypt.compare(plainPassword, user.password);
+    return isMatch;
+  } catch (err) {
+    return err;
+  }
+};
 
-module.exports=(dbName)=>{
-    return conn[dbName].model('User',userSchema);
-}
+module.exports = (dbName) => {
+  return conn[dbName].model("User", userSchema);
+};
