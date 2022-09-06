5d837d43bc62d16a990a95b9e9259a46438357d6
seedlessapple <luminousseedlessapple@gmail.com>
2022-09-06 11:04:04 +0900

    Update Index.tsx

--- frontend/src/pages/settings/Index.tsx
+++ frontend/src/pages/settings/Index.tsx
@@ -7,14 +7,42 @@ type Props = {};
 
 const Settings = (props: Props) => {
   const { currentUser } = useAuth();
-  return (
-    <div className={style.section}>
-      <NavigationLinks />
-      <div className={style.title}>설정</div>
-      <div className={style.user_container}>
-        <div className={style.username}>{currentUser.userName}</div>
+
+  const SettingItem = () => {
+    return (
+      <div className={style.setting_item}>
+        <div className={style.title}>이름</div>
+        <input className={style.input} type="text" placeholder="이름" />
       </div>
-    </div>
+    );
+  };
+  const SettingsContainer = ({
+    children,
+  }: {
+    children: JSX.Element | JSX.Element[];
+  }) => {
+    return (
+      <div className={style.settings_container}>
+        <div className={style.container_title}>사용자 정보</div>
+        {children}
+      </div>
+    );
+  };
+
+  return (
+    <>
+      <div className={style.search_container}>
+        <div className={style.title}>사용자 설정</div>
+        <input className={style.search} type="text" placeholder="검색" />
+      </div>
+      <div className={style.section}>
+        <SettingsContainer>
+          <SettingItem />
+          <SettingItem />
+          <SettingItem />
+        </SettingsContainer>
+      </div>
+    </>
   );
 };
 
