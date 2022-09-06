4c3ef43f997cc826db3ec6ee29d0d63c88348b32
seedlessapple <luminousseedlessapple@gmail.com>
2022-09-07 02:16:49 +0900

    Create SocialLoginSettings.tsx

---
+++ frontend/src/pages/settings/tabs/SocialLoginSettings.tsx
@@ -0,0 +1,55 @@
+import Svg from "../../../assets/svg/Svg";
+import Button from "../../../components/button/Button";
+import { useAuth } from "../../../contexts/authContext";
+import style from "../../../style/pages/settings/settings.module.scss";
+
+type Props = {};
+
+const SocialLoginSettings = (props: Props) => {
+  const { currentUser } = useAuth();
+  return (
+    <div className={style.settings_container}>
+      <div className={style.container_title}>소셜 로그인 연동</div>
+      {currentUser.snsId?.map((value: any, index: number) => {
+        return (
+          <div
+            key={index}
+            className={style.setting_item}
+            style={{
+              padding: "12px 16px",
+              border: "var(--border-default)",
+              borderRadius: "8px",
+            }}
+          >
+            <div className={style.icon}>
+              <Svg type="google" width="26px" height="26px"/>
+            </div>
+            <div className={style.info}>
+              <label className={style.label}>{value.provider}</label>
+              <span className={style.description}>{value.email}</span>
+            </div>
+            <div
+              className={style.controls}
+              style={{ alignItems: "flex-start", cursor: "pointer" }}
+            >
+              <div style={{}}>
+                <Svg type="verticalDots" />
+              </div>
+            </div>
+          </div>
+        );
+      })}
+      <Button
+        type={"ghost"}
+        styles={{
+          borderRadius: "4px",
+          height: "32px",
+        }}
+      >
+        연동하기
+      </Button>
+    </div>
+  );
+};
+
+export default SocialLoginSettings;
