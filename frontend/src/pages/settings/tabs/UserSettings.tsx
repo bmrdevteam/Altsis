b197e164a40c900442f6b4f95d19ed4e03c6ca1a
seedlessapple <luminousseedlessapple@gmail.com>
2022-09-06 13:00:00 +0900

    Create UserSettings.tsx

---
+++ frontend/src/pages/settings/tabs/UserSettings.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Input from "../../../components/input/Input";
+import Textarea from "../../../components/textarea/Textarea";
+import { useAuth } from "../../../contexts/authContext";
+import style from "../../../style/pages/settings/settings.module.scss";
+
+type Props = {};
+
+const UserSettings = (props: Props) => {
+    const {currentUser} = useAuth()
+  return (
+    <div className={style.settings_container}>
+      <div className={style.container_title}>사용자 정보</div>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <div style={{ display: "flex" ,gap:"12px"}}>
+        <Input
+          label="이름"
+          placeholder="이름"
+          inputStyle="flat"
+          style={{ fontSize: "14px" }}
+          defaultValue={currentUser.userName}
+        />
+        <Input
+          label="아이디"
+          placeholder="아이디"
+          inputStyle="flat"
+          style={{ fontSize: "14px" }}
+          defaultValue={currentUser.userId}
+          disabled
+        />
+      </div>
+    </div>
+  );
+};
+
+export default UserSettings;
