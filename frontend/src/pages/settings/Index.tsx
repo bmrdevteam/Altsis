dd1df996ae4e049dfb69d37c8a9587a4817ae202
seedlessapple <luminousseedlessapple@gmail.com>
2022-09-06 12:59:56 +0900

    Update Index.tsx

--- frontend/src/pages/settings/Index.tsx
+++ frontend/src/pages/settings/Index.tsx
@@ -1,35 +1,16 @@
 import React from "react";
 import Input from "../../components/input/Input";
-import NavigationLinks from "../../components/navigationLinks/NavigationLinks";
 import Textarea from "../../components/textarea/Textarea";
 import { useAuth } from "../../contexts/authContext";
 import style from "../../style/pages/settings/settings.module.scss";
+import ThemeSettings from "./tabs/ThemeSettings";
+import UserSettings from "./tabs/UserSettings";
 
 type Props = {};
 
 const Settings = (props: Props) => {
   const { currentUser } = useAuth();
 
-  const SettingItem = () => {
-    return (
-      <div className={style.setting_item}>
-        <div className={style.title}>이름</div>
-        <input className={style.input} type="text" placeholder="이름" />
-      </div>
-    );
-  };
-  const SettingsContainer = ({
-    children,
-  }: {
-    children: JSX.Element | JSX.Element[];
-  }) => {
-    return (
-      <div className={style.settings_container}>
-        <div className={style.container_title}>사용자 정보</div>
-        {children}
-      </div>
-    );
-  };
 
   return (
     <>
@@ -38,10 +19,8 @@ const Settings = (props: Props) => {
         <input className={style.search} type="text" placeholder="검색" />
       </div>
       <div className={style.section}>
-        <SettingsContainer>
-          <Input label="이름" placeholder="이름" inputStyle="flat" style={{fontSize:"14px"}}/>
-          <Textarea />
-        </SettingsContainer>
+         <UserSettings/>
+         <ThemeSettings/>
       </div>
     </>
   );
