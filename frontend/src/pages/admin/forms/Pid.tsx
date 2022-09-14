5656bfaca11de0118120cd6592be7b36f3874771
seedlessapple <luminousseedlessapple@gmail.com>
2022-09-14 15:19:30 +0900

    Update Pid.tsx

--- frontend/src/pages/admin/forms/Pid.tsx
+++ frontend/src/pages/admin/forms/Pid.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
 import Editor from "../../../components/editor/Editor";
 import useEditor from "../../../hooks/useEditor";
 
@@ -7,11 +7,13 @@ import useDatabase from "../../../hooks/useDatabase";
 import style from "../../../style/pages/admin/forms/forms.module.scss";
 import Divider from "../../../components/divider/Divider";
 import ReloadWarning from "../../../components/reloadwarning/ReloadWarning";
+import Svg from "../../../assets/svg/Svg";
 
 type Props = {};
 
 const Form = (props: Props) => {
   const { pid } = useParams<"pid">();
+  const navigate = useNavigate();
   const database = useDatabase();
   const editor = useEditor();
 
@@ -42,7 +44,33 @@ const Form = (props: Props) => {
   }, [updateFormData]);
 
   return (
-    <div>
+    <div
+      style={{
+        position: "fixed",
+        width: "100vw",
+        top: "0",
+        left: "0",
+        zIndex: "3000",
+        background: "var(--background-color)",
+      }}
+    >
+      <div className={style.header_container}>
+        <div className={style.header}>
+          <div
+            className={style.back}
+            onClick={() => {
+              navigate(-1);
+            }}
+          >
+            <Svg type={"arrowBack"} width="20px" height="20px" />
+          </div>
+          <div className={style.title}>
+            <input defaultValue={formData?.title} />
+          </div>
+          <div className={style.save}>저장</div>
+        </div>
+      </div>
+
       <Editor auth="edit" editorhook={editor} initalData={formData?.data} />
     </div>
   );
