3091d8b3b2089e0d2c3f2508f6ced333954da027
seedlessapple <luminousseedlessapple@gmail.com>
2022-09-15 23:47:03 +0900

    Create Sidebar.tsx

---
+++ frontend/src/components/editor/menu/Sidebar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import style from "../editor.module.scss";
+
+type Props = {};
+
+function Sidebar({}: Props) {
+  return <div className={style.sidebar_container}>Sidebar</div>;
+}
+
+export default Sidebar;
