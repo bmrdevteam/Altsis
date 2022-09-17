39b8978ea20fbacf42228d67ada5cd88aa1cf560
seedlessapple <luminousseedlessapple@gmail.com>
2022-09-17 23:26:15 +0900

    Update Sidebar.tsx

--- frontend/src/components/editor/menu/Sidebar.tsx
+++ frontend/src/components/editor/menu/Sidebar.tsx
@@ -2,9 +2,26 @@ import React from "react";
 import style from "../editor.module.scss";
 
 type Props = {};
-
+/**
+ *
+ * @param param0
+ *
+ *
+ * @returns
+ */
 function Sidebar({}: Props) {
-  return <div className={style.sidebar_container}>Sidebar</div>;
+  return (
+    <div className={style.sidebar_container}>
+      <div className={style.sidebar}>
+        <div className={style.item}>
+          <div className={style.label}>텍스트</div>
+          <div className={style.content}>
+            <div> </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
