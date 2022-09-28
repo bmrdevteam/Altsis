778502225f003b380e4b9e66445f455f4e670c0b
seedlessapple <luminousseedlessapple@gmail.com>
2022-09-28 14:35:58 +0900

    Create InputBlock.tsx

---
+++ frontend/src/editor/blocks/InputBlock.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Input from "../../components/input/Input";
+import style from "../editor.module.scss";
+import { useEditor } from "../functions/editorContext";
+
+type Props = { index: number };
+
+const InputBlock = (props: Props) => {
+  const { getBlock } = useEditor();
+
+  const block = getBlock(props.index);
+
+  return (
+    <div className={style.block}>
+      <Input label={block.data.label} required={block.data.required} placeholder={block.data.placeholder} />
+    </div>
+  );
+};
+
+export default InputBlock;
