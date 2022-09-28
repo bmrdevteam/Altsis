de0c940fb34870c177ad747a28557fcd0b4992e6
seedlessapple <luminousseedlessapple@gmail.com>
2022-09-28 14:35:41 +0900

    Update Block.tsx

--- frontend/src/components/editor/Block.tsx
+++ frontend/src/components/editor/Block.tsx
@@ -43,7 +43,7 @@ const Block = ({
   editorId,
   contextMenuController,
 }: {
-  data: IBlock;
+  data: any;
   editorId: string;
   contextMenuController: any;
 }) => {
@@ -51,15 +51,15 @@ const Block = ({
 
   const block = data as IParagraphBlock;
 
-  const Block = ({ children }: { children?: JSX.Element }) => {
-    let classNameArr: string[] = [style.block];
-    if (block.type === "table") {
-      classNameArr.push(style.table);
-    }
-    if (block.type === "divider") {
-      classNameArr.push(style.divider);
-    }
+  let classNameArr: string[] = [style.block];
 
+  if (block.type === "table") {
+    classNameArr.push(style.table);
+  }
+  if (block.type === "divider") {
+    classNameArr.push(style.divider);
+  }
+  const Block = ({ children }: { children?: JSX.Element }) => {
     return (
       <div
         id={`${editorId}-${data.id}`}
@@ -68,6 +68,11 @@ const Block = ({
         data-divide-count={1}
         contentEditable
         suppressContentEditableWarning
+        onClick={() => {
+          console.log("set", `${editorId}-${data.id}`);
+
+          editor.setCurrentBlockId(`${editorId}-${data.id}`);
+        }}
         onInput={(e: any) => {
           editor.saveBlock({
             block: {
@@ -79,25 +84,25 @@ const Block = ({
           });
         }}
         onKeyDown={(e: any) => {
-          // styleFunctions.handleKeyDown(e);
+          editor.handleKeyDown(e);
 
           if (e.key === "Tab") {
             e.preventDefault();
           }
-          if (e.key === "Enter") {
-            e.preventDefault();
-            editor.addBlock({
-              insertAfter: editor.getBlockIndex(block.id) + 1,
-            });
+          // if (e.key === "Enter") {
+          //   e.preventDefault();
+          //   editor.addBlock({
+          //     insertAfter: editor.getBlockIndex(block.id) + 1,
+          //   });
 
-            editor.saveBlock({
-              block: {
-                id: block.id,
-                type: block.type,
-                data: { text: e.target.innerHTML },
-              },
-            });
-          }
+          //   editor.saveBlock({
+          //     block: {
+          //       id: block.id,
+          //       type: block.type,
+          //       data: { text: e.target.innerHTML },
+          //     },
+          //   });
+          // }
 
           if (
             editor.result().length > 1 &&
@@ -122,7 +127,45 @@ const Block = ({
 
   switch (data.type) {
     case "input":
-      return <Input label="입력의 이름을 정해주세요" required/>;
+      const inputLabel = data.data.label ?? "입력";
+      const inputPlaceholder = data.data.placeholder ?? "입력하세요";
+      const inputRequired = data.data.required ?? true;
+      return (
+        <div
+          id={`${editorId}-${data.id}`}
+          className={classNameArr.join(" ")}
+          placeholder={"입력"}
+          data-divide-count={1}
+          onClick={() => {
+            editor.setCurrentBlockId(`${editorId}-${data.id}`);
+          }}
+          onKeyDown={(e: any) => {
+            editor.handleKeyDown(e);
+
+            if (
+              editor.result().length > 1 &&
+              e.key === "Backspace" &&
+              e.target.innerText === ""
+            ) {
+              editor.deleteBlock(block.id);
+            }
+          }}
+          // onContextMenu={(e) => {
+          //   e.preventDefault();
+          //   contextMenuController({
+          //     position: [e.pageX, e.pageY],
+          //     ref: e,
+          //     blockId: block.id,
+          //   });
+          // }}
+        >
+          <Input
+            label={inputLabel}
+            required={inputRequired}
+            placeholder={inputPlaceholder}
+          />
+        </div>
+      );
     default:
       return <Block />;
   }
