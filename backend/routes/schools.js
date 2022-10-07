a1faba3de35875ffa915726d588e22c38a764694
jessie129j@gmail.com <jessie129j@gmail.com>
2022-10-07 16:03:46 +0900

    update School APIs

--- backend/routes/schools.js
+++ backend/routes/schools.js
@@ -1,28 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const school = require("../controllers/school");
-const { isLoggedIn, isAdManager } = require("../middleware/auth");
+const { isAdManager } = require("../middleware/auth");
 
 //=================================
 //             School
 //=================================
 
-/* basic create, read, delete */
 router.post("/", isAdManager, school.create);
-router.get("/", isAdManager, school.list);
-router.get("/:_id/:field?", isAdManager, school.read);
+
+router.get("/:_id?/:field?", isAdManager, school.find);
+
+router.put("/:_id/subjects", isAdManager, school.updateField);
+router.put("/:_id/classrooms", isAdManager, school.updateClassrooms);
+router.put("/:_id/formArchive", isAdManager, school.updateFormArchive);
+
 router.delete("/:_id", isAdManager, school.delete);
-
-/* settings */
-router.put("/:_id/settings", isAdManager, school.updateSettings);
-
-/* classrooms, subjects, seasons */
-router.post("/:_id/:field", isLoggedIn, school.pushField);
-router.put("/:_id/:field", isAdManager, school.updateField);
-router.put("/:_id/:field/:idx", isAdManager, school.updateFieldByIdx);
-router.delete("/:_id/:field/:idx", isAdManager, school.deleteFieldByIdx);
-
-/* basic update */
-router.put("/:_id/:field?", isAdManager, school.update);
 
 module.exports = router;
