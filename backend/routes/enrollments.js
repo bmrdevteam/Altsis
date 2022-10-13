9c40371c723aa483ed61ad4e43045bc0456989c3
jessie129j@gmail.com <jessie129j@gmail.com>
2022-10-13 15:15:02 +0900

    update Enrollment APIs

--- backend/routes/enrollments.js
+++ backend/routes/enrollments.js
@@ -1,18 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const enrollment = require("../controllers/enrollment");
-const { isAdmin, isLoggedIn } = require("../middleware/auth");
+const { isAdmin, isLoggedIn, isAdManager } = require("../middleware/auth");
 
 //=================================
 //             Enrollment
 //=================================
 
-router.post("/", isLoggedIn, enrollment.create);
-router.post("/bulk", isLoggedIn, enrollment.createBulk);
+router.post("/", isLoggedIn, enrollment.enroll);
+router.post("/bulk", isAdManager, enrollment.enrollbulk);
 
-router.get("/list", isLoggedIn, enrollment.getAll);
+router.get("/students", isLoggedIn, enrollment.findStudents);
+
+router.get("/evaluations", isLoggedIn, enrollment.findStudentsWithEvaluation);
 
 router.put("/:_id/evaluation", isLoggedIn, enrollment.updateEvaluation);
+router.get("/:_id?", isLoggedIn, enrollment.find);
+
 router.delete("/:_id", isLoggedIn, enrollment.remove);
 
 module.exports = router;
