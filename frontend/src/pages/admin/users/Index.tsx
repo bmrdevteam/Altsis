31c372141641258adcc08796b9cafce45d89a651
seedlessapple <luminousseedlessapple@gmail.com>
2022-10-23 22:40:45 +0900

    Update Index.tsx

--- frontend/src/pages/admin/users/Index.tsx
+++ frontend/src/pages/admin/users/Index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { text } from "stream/consumers";
-import FIleUploader from "../../../components/fileUploader/FIleUploader";
+import Button from "../../../components/button/Button";
+
 import Input from "../../../components/input/Input";
 import NavigationLinks from "../../../components/navigationLinks/NavigationLinks";
 import Popup from "../../../components/popup/Popup";
@@ -16,20 +16,18 @@ type Props = {};
 const Users = (props: Props) => {
   const database = useDatabase();
   const [academyUsers, setAcademyUsers] = useState<any>();
-  const [schoolUsers, setSchoolUsers] = useState<any>();
-  const [moreInfo, setMoreInfo] = useState<any>();
+  const [user, setUser] = useState<any>();
+  const [userRegistrations, setUserRegistrations] = useState<any[]>();
+  console.log(user);
 
   const [userInfoPopupActive, setUserInfoPopupActive] =
     useState<boolean>(false);
   async function getAcademyUsers() {
-    const { users: res } = await database.R({ location: "users/members" });
+    const { users: res } = await database.R({ location: "users" });
     return res;
   }
-
-  async function getSchoolUsers() {
-    const { schoolUsers: res } = await database.R({
-      location: "schoolusers/list",
-    });
+  async function getUserRegistrations(id: string) {
+    const res = await database.R({ location: `registrations?userId=${id}` });
     return res;
   }
 
@@ -37,10 +35,7 @@ const Users = (props: Props) => {
     getAcademyUsers().then((res) => {
       setAcademyUsers(res);
     });
-    getSchoolUsers().then((res) => setSchoolUsers(res));
-    return () => {};
   }, []);
-  console.log(academyUsers);
 
   const search = useSearch(academyUsers);
 
@@ -50,36 +45,18 @@ const Users = (props: Props) => {
         <NavigationLinks />
         <div className={style.title}>아카데미 유저 관리</div>
         <div className={style.filter_container}>
-          <div className={style.filter}>
-            <div>
-              <Input
-                placeholder="검색"
-                onChange={(e: any) => {
-                  search.addFilterItem({
-                    id: "search",
-                    key: "userName",
-                    operator: "=",
-                    value: e.target.value,
-                  });
-                }}
-              />
-            </div>
-            {/* <div style={{ display: "flex" }}>
-              <Select
-                options={[
-                  { text: "ID", value: "id" },
-                  { text: "Name", value: "name" },
-                  { text: "Role", value: "role" },
-                ]}
-              />
-
-              <Select
-                options={[
-                  { text: "=", value: "=" },
-                  { text: ">", value: ">" },
-                ]}
-              />
-            </div> */}
+          <div>
+            <Input
+              placeholder="검색"
+              onChange={(e: any) => {
+                search.addFilterItem({
+                  id: "search",
+                  key: "userName",
+                  operator: "=",
+                  value: e.target.value,
+                });
+              }}
+            />
           </div>
         </div>
         <div style={{ height: "24px" }}></div>
@@ -110,23 +87,28 @@ const Users = (props: Props) => {
                 align: "center",
               },
               {
+                text: "auth",
+                key: "auth",
+                type: "string",
+                align: "center",
+              },
+              {
                 text: "자세히",
                 key: "_id",
                 type: "button",
                 onClick: (e: any) => {
-                  setMoreInfo([
+                  setUser(
                     academyUsers?.filter(
                       (val: any) => val._id === e.target.dataset.value
-                    )[0],
-                    schoolUsers?.filter(
-                      (val: any) =>
-                        val.userId ===
-                        academyUsers?.filter(
-                          (val: any) => val._id === e.target.dataset.value
-                        )[0].userId
-                    ),
-                  ]);
-
+                    )[0]
+                  );
+                  getUserRegistrations(
+                    academyUsers?.filter(
+                      (val: any) => val._id === e.target.dataset.value
+                    )[0].userId
+                  ).then((res) => {
+                    setUserRegistrations(res);
+                  });
                   setUserInfoPopupActive(true);
                 },
                 width: "72px",
@@ -137,31 +119,54 @@ const Users = (props: Props) => {
         </div>
       </div>
       {userInfoPopupActive && (
-        <Popup setState={setUserInfoPopupActive} title="유저 정보">
+        <Popup
+          closeBtn
+          setState={setUserInfoPopupActive}
+          title="유저 정보"
+          style={{ borderRadius: "4px" }}
+        >
           <div className={style.popup}>
-            <div
-              style={{ display: "flex", gap: "12px" }}
-              onClick={() => {
-
-              }}
-            >
+            <div className={style.title}>기본 정보</div>
+            <div className={style.row}>
               <Input
+                inputStyle="flat"
                 label="이름"
                 required
-                defaultValue={moreInfo[0].userName}
+                defaultValue={user.userName}
               />
-              <Input label="Id" required defaultValue={moreInfo[0].userId} />
-            </div>
-            <div>
-              <Select
-              label="학교"
-                options={Array.from(
-                  moreInfo[1].map((value: any, index: number) => {
-                    return { text: value.schoolName, value: index };
-                  })
-                )}
+              <Input
+                label="Id"
+                required
+                defaultValue={user.userId}
+                inputStyle="flat"
               />
             </div>
+            <div className={style.row}>
+              <Input
+                inputStyle="flat"
+                label="이메일"
+                required
+                defaultValue={user.email}
+              />
+              <Input
+                label="tel"
+                required
+                defaultValue={user.tel}
+                inputStyle="flat"
+              />
+            </div>
+            <Button
+              type={"ghost"}
+              disableOnclick
+              onClick={() => {}}
+              styles={{
+                borderRadius: "4px",
+                height: "32px",
+                boxShadow: "rgba(0, 0, 0, 0.1) 0px 1px 2px 0px",
+              }}
+            >
+              저장
+            </Button>
           </div>
         </Popup>
       )}
