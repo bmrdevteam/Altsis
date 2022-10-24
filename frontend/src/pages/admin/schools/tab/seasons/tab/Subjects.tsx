63eed6c5fcb3d75c25f5bb84748b24a479bbe435
seedlessapple <luminousseedlessapple@gmail.com>
2022-10-24 13:19:50 +0900

    Create Subjects.tsx

---
+++ frontend/src/pages/admin/schools/tab/seasons/tab/Subjects.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+type Props = {
+    seasonData: any;
+  };
+const Subjects = (props: Props) => {
+  return (
+    <div>Subjects</div>
+  )
+}
+
+export default Subjects
