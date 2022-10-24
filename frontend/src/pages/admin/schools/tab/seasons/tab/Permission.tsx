631bd646684510b8817a73289d54018a175305fe
seedlessapple <luminousseedlessapple@gmail.com>
2022-10-24 13:20:35 +0900

    Create Permission.tsx

---
+++ frontend/src/pages/admin/schools/tab/seasons/tab/Permission.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+type Props = {
+    seasonData: any;
+  };
+const Permission = (props: Props) => {
+  return (
+    <div>Permission</div>
+  )
+}
+
+export default Permission
