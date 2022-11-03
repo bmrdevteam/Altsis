78dba32344000b78f09b7b8f8484a82618891f2e
seedlessapple <luminousseedlessapple@gmail.com>
2022-11-03 14:08:17 +0900

    Update ParsedBlock.tsx

--- frontend/src/editor/parser/blocks/ParsedBlock.tsx
+++ frontend/src/editor/parser/blocks/ParsedBlock.tsx
@@ -5,6 +5,8 @@ import ParsedTableBlock from "./ParsedTableBlock";
 type Props = {
   blockData: any;
   onChange?: (data: any) => void;
+  auth: "edit" | "view";
+  returnData: any;
 };
 
 const ParsedBlock = (props: Props) => {
@@ -16,9 +18,17 @@ const ParsedBlock = (props: Props) => {
         <div className={style.parsed_block}>{props.blockData.data.text}</div>
       );
     case "table":
-      return <ParsedTableBlock blockData={props.blockData} />;
+      return (
+        <ParsedTableBlock
+          blockData={props.blockData}
+          auth={props.auth}
+          returnData={props.returnData}
+        />
+      );
     case "dataTable":
-      return <ParsedDataTableBlock blockData={props.blockData} />;
+      return (
+        <ParsedDataTableBlock blockData={props.blockData} auth={props.auth} />
+      );
     default:
       return (
         <div className={style.parsed_block}>{props.blockData.data.text}</div>
