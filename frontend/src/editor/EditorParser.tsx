ecaf580f6de0a8fb85b81229056caed6e001e4fc
seedlessapple <luminousseedlessapple@gmail.com>
2022-11-03 14:08:19 +0900

    Update EditorParser.tsx

--- frontend/src/editor/EditorParser.tsx
+++ frontend/src/editor/EditorParser.tsx
@@ -1,21 +1,34 @@
 import { isArray } from "lodash";
+import { useRef } from "react";
 import ParsedBlock from "./parser/blocks/ParsedBlock";
 
 type Props = {
   data: any;
   onChange?: (data: any) => {};
+  auth: "edit" | "view";
 };
 
 const EditorParser = (props: Props) => {
   console.log(props.data);
+  const returnData = useRef<any>({});
 
   return (
-    <div>
+    <div
+      onInput={() => {
+        console.log(returnData.current);
+        props.onChange && props.onChange(returnData.current);
+      }}
+    >
       {isArray(props?.data?.data) &&
         props?.data?.data?.map((value: any, index: number) => {
           return (
             <div key={index}>
-              <ParsedBlock blockData={value} onChange={props.onChange} />
+              <ParsedBlock
+                returnData={returnData.current}
+                blockData={value}
+                auth={props.auth}
+                onChange={props.onChange}
+              />
             </div>
           );
         })}
