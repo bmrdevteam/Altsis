8b7cf04d53dd7e6c5df9fe8d6ac943c08e71a3eb
seedlessapple <luminousseedlessapple@gmail.com>
2022-11-03 14:08:01 +0900

    Update SelectCell.tsx

--- frontend/src/editor/blocks/table/SelectCell.tsx
+++ frontend/src/editor/blocks/table/SelectCell.tsx
@@ -12,11 +12,8 @@ const SelectCell = (props: Props) => {
   const { getCell } = useEditor();
   const cell = getCell(props.blockIndex, props.row, props.column);
   return (
-    <div
-      className={`${style.cell} ${style.select}`}
-      style={{ textAlign: cell?.align }}
-    >
-      <select>
+    <div className={`${style.cell} ${style.select}`}>
+      <select style={{ textAlign: cell?.align, fontSize: cell?.fontSize }}>
         {cell?.options?.map((value: any) => {
           return (
             <option key={value.id} value={value.value}>
