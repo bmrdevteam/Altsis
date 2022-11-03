13216f0e861a953e87c6bb33b74faedd488923dc
seedlessapple <luminousseedlessapple@gmail.com>
2022-11-03 10:04:14 +0900

    Update E.tsx

--- frontend/src/pages/dev/E.tsx
+++ frontend/src/pages/dev/E.tsx
@@ -1,15 +1,10 @@
-import React from "react";
 
-import NavigationLinks from "../../components/navigationLinks/NavigationLinks";
-import Editor from "../../editor/Editor";
-import EditorParser from "../../editor/EditorParser";
 
 type Props = {};
 
 const E = (props: Props) => {
   return (
     <div>
-      <EditorParser id="634c44d0f2f3ab456b4a94fa"/>
     </div>
   );
 };
