d055c7d6224679485a67d63dc76375fdac70b398
seedlessapple <luminousseedlessapple@gmail.com>
2022-11-15 12:21:38 +0900

    Create archiveTest.ts

---
+++ frontend/src/archiveTest.ts
@@ -0,0 +1,52 @@
+export const archiveTestData = [
+    {
+      label: "인적사항",
+      cycle: "unset",
+      fields: [
+        {
+          label: "성명",
+        },
+        {
+          label: "성별",
+        },
+        {
+          label: "주민등록번호",
+        },
+        {
+          label: "주소",
+        },
+        {
+          label: "성명(부)",
+        },
+        {
+          label: "성명(모)",
+        },
+
+        {
+          label: "생년월일(부)",
+        },
+        {
+          label: "생년월일(모)",
+        },
+        {
+          label: "특기사항",
+        },
+      ],
+    },
+    {
+      label: "학적사항",
+      cycle: "unset",
+    },
+    {
+      label: "수상경력",
+      cycle: "unset",
+    },
+    {
+      label: "자격증 및 취득사항",
+      cycle: "unset",
+    },
+    {
+      label: "독서 활동",
+      cycle: "year",
+    },
+  ];
