9d351c165acf36d4848f9a9d3dc66dbdec5e7615
seedlessapple <luminousseedlessapple@gmail.com>
2022-11-16 18:14:27 +0900

    Update Index.tsx

--- frontend/src/pages/archive/Index.tsx
+++ frontend/src/pages/archive/Index.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
-
-type Props = {}
+import Navbar from "layout/navbar/Navbar";
+import React from "react";
+import style from "style/pages/archive.module.scss";
+type Props = {};
 
 const Archive = (props: Props) => {
   return (
-    <div>Archive</div>
-  )
-}
+    <>
+      <Navbar />
+      <div className={style.section}>Archive</div>
+    </>
+  );
+};
 
-export default Archive
+export default Archive;
