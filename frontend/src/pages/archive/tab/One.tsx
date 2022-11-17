e8662c0e3fc29c5ba127be7faa09b2e414d7ee30
seedlessapple <luminousseedlessapple@gmail.com>
2022-11-17 12:30:12 +0900

    Update One.tsx

--- frontend/src/pages/archive/tab/One.tsx
+++ frontend/src/pages/archive/tab/One.tsx
@@ -1,14 +1,22 @@
 import Autofill from "components/input/Autofill";
 import React from "react";
 import style from "style/pages/archive.module.scss";
-type Props = {};
+type Props = {
+  users: any[];
+};
 
 const One = (props: Props) => {
+
   return (
     <div style={{ marginTop: "24px" }}>
       <div className={style.search}>
         <div className={style.label}>학생선택</div>
-        <Autofill appearence="flat" options={[]} />
+        <Autofill
+          appearence="flat"
+          options={props.users.map((val) => {
+            return { text: val.userName, value: val.userId };
+          })}
+        />
       </div>
     </div>
   );
