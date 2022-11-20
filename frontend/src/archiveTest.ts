44bad223770bfa2a0323a688d39d525c356a4918
seedlessapple <luminousseedlessapple@gmail.com>
2022-11-20 11:57:00 +0900

    Update archiveTest.ts

--- frontend/src/archiveTest.ts
+++ frontend/src/archiveTest.ts
@@ -1,52 +1,79 @@
 export const archiveTestData = [
-    {
-      label: "인적사항",
-      cycle: "unset",
-      fields: [
-        {
-          label: "성명",
-        },
-        {
-          label: "성별",
-        },
-        {
-          label: "주민등록번호",
-        },
-        {
-          label: "주소",
-        },
-        {
-          label: "성명(부)",
-        },
-        {
-          label: "성명(모)",
-        },
+  {
+    label: "인적사항",
+    cycle: "unset",
+    dataType: "single",
+    fields: [
+      {
+        label: "성명",
+      },
+      {
+        label: "성별",
+      },
+      {
+        label: "주민등록번호",
+      },
+      {
+        label: "주소",
+      },
+      {
+        label: "성명(부)",
+      },
+      {
+        label: "성명(모)",
+      },
 
-        {
-          label: "생년월일(부)",
-        },
-        {
-          label: "생년월일(모)",
-        },
-        {
-          label: "특기사항",
-        },
-      ],
-    },
-    {
-      label: "학적사항",
-      cycle: "unset",
-    },
-    {
-      label: "수상경력",
-      cycle: "unset",
-    },
-    {
-      label: "자격증 및 취득사항",
-      cycle: "unset",
-    },
-    {
-      label: "독서 활동",
-      cycle: "year",
-    },
-  ];
+      {
+        label: "생년월일(부)",
+      },
+      {
+        label: "생년월일(모)",
+      },
+      {
+        label: "특기사항",
+      },
+    ],
+  },
+  {
+    label: "학적사항",
+    cycle: "unset",
+    dataType: "multiple",
+  },
+  {
+    label: "수상경력",
+    cycle: "unset",
+    dataType: "multiple",
+  },
+  {
+    label: "자격증 및 취득사항",
+    cycle: "unset",
+    dataType: "multiple",
+  },
+  {
+    label: "독서 활동",
+    cycle: "year",
+    dataType: "multiple",
+  },
+  {
+    label: "봉사 활동",
+    cycle: "unset",
+    dataType: "multiple",
+    fields: [
+      {
+        label: "학년",
+      },
+      {
+        label: "일자 또는 기간",
+      },
+      {
+        label: "장소 또는 주관기관명",
+      },
+      {
+        label: "활동내용",
+      },
+      {
+        label: "시간",
+      },
+    ],
+  },
+];
