894c8557ae2dc9cd03b1dae8c2d16608c7b262cf
seedlessapple <luminousseedlessapple@gmail.com>
2022-11-20 11:58:17 +0900

    Update Autofill.tsx

--- frontend/src/components/input/Autofill.tsx
+++ frontend/src/components/input/Autofill.tsx
@@ -1,26 +1,29 @@
 import React, { useEffect, useRef, useState } from "react";
 import style from "./autoFill.module.scss";
 type Props = {
-  options:
-    | {
-        text: string;
-        value: string | number;
-      }[];
-
+  options: {
+    text: string;
+    value: string | number;
+  }[];
   style?: any;
   ref?: any;
   label?: string;
   required?: boolean;
-  defaultSelected?: number;
+  defaultValue?: any;
+  placeholder?: string;
   setValue?: any;
+  setState?: React.Dispatch<React.SetStateAction<any>>;
   appearence?: "flat";
 };
 
 const Autofill = (props: Props) => {
-  const [selected, setSelected] = useState<number>(
-    props.defaultSelected ? props.defaultSelected : 0
+  const [inputValue, setInputValue] = useState<string>(
+    (props.defaultValue &&
+      props.options.filter((val) => {
+        return val.value.toString() === props.defaultValue.toString();
+      })[0].text) ??
+      ""
   );
-  const [inputValue, setInputValue] = useState(props.options[selected]?.text);
   const [valid, setValid] = useState(true);
 
   const [edit, setEdit] = useState<boolean>(false);
@@ -33,33 +36,24 @@ const Autofill = (props: Props) => {
   }
   useEffect(() => {
     document.addEventListener("mousedown", handleMousedown);
-    props.defaultSelected && setSelected(props.defaultSelected);
-    props.setValue && props.setValue(props.options[selected].value);
+    props.setValue &&
+      props.setValue(
+        props.defaultValue &&
+          props.options.filter((val) => {
+            return val.value.toString() === props.defaultValue.toString();
+          })[0]?.value
+      );
+
+    setInputValue(
+      props.options.filter((val) => {
+        return val.value.toString() === props.defaultValue.toString();
+      })[0]?.text ?? ""
+    );
 
     return () => {
       document.removeEventListener("mousedown", handleMousedown);
     };
   }, []);
-
-  useEffect(() => {
-    if (
-      props.options.filter((val: { text: string; value: string | number }) => {
-        if (inputValue === "") {
-          return val;
-        } else if (val.text.toLowerCase().includes(inputValue.toLowerCase())) {
-          return val;
-        }
-      }).length === 0
-    ) {
-      setValid(false);
-    } else {
-      setValid(true);
-    }
-
-    if (props.required && inputValue === "") {
-      setValid(false);
-    }
-  }, [inputValue]);
 
   return (
     <div
@@ -77,38 +71,78 @@ const Autofill = (props: Props) => {
       )}
       <input
         style={{
+          borderRadius: props.style?.borderRadius,
           borderColor: !valid
             ? "var(--alert-c1)"
             : "var(--border-default-color)",
         }}
+        placeholder={props.placeholder}
         type="text"
-        // value={}
+        value={inputValue}
         onChange={(e) => {
           setInputValue(e.target.value);
-        }}
-        onKeyDown={(e) => {
-          if (e.key === "Enter") {
-            setEdit(false);
+          if (
+            props.options.filter(
+              (val: { text: string; value: string | number }) => {
+                if (e.target.value === "") {
+                  return val;
+                } else if (
+                  val.text.toLowerCase().includes(e.target.value.toLowerCase())
+                ) {
+                  return val;
+                }
+              }
+            ).length === 0
+          ) {
+            setValid(false);
+          } else {
+            setValid(true);
+          }
+
+          if (props.required && e.target.value === "") {
+            setValid(false);
           }
         }}
+        onKeyDown={(e) => {
+          // const inputText = e.currentTarget.value;
+          // if (e.key === "Enter" || e.key === "Tab") {
+          //   e.preventDefault();
+          //   const newInput =
+          //     props.options.filter(
+          //       (val: { text: string; value: string | number }) => {
+          //         return val.text
+          //           .toLowerCase()
+          //           ?.includes(inputText.toLowerCase());
+          //       }
+          //     )[0]?.text ?? "";
+          //   setInputValue(newInput);
+          // }
+        }}
         className={style.input}
-        value={inputValue}
         onFocus={() => {
           setEdit(true);
         }}
       />
 
-      {edit && (valid || inputValue === "") && (
-        <div className={style.options}>
+      {edit && (valid || inputValue !== "") && (
+        <div
+          className={style.options}
+          style={{
+            borderTop: "none",
+            borderRadius: props.style?.borderRadius,
+          }}
+        >
           {props.options
             .filter((val: { text: string; value: string | number }) => {
               if (inputValue === "") {
-                return val;
+                return true;
               } else if (
-                val.text.toLowerCase().includes(inputValue.toLowerCase())
+                val.text &&
+                val.text?.toLowerCase()?.includes(inputValue?.toLowerCase())
               ) {
-                return val;
+                return true;
               }
+              return false;
             })
             .map((value, index) => {
               return (
@@ -116,6 +150,7 @@ const Autofill = (props: Props) => {
                   key={index}
                   onClick={() => {
                     setInputValue(value.text);
+                    props.setState && props.setState(value.value);
                     setEdit(false);
                   }}
                   className={style.option}
