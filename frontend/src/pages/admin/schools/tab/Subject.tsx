bc8038adef1259cb5dec3132c9882e2b64b38839
seedlessapple <luminousseedlessapple@gmail.com>
2022-11-20 11:57:23 +0900

    Update Subject.tsx

--- frontend/src/pages/admin/schools/tab/Subject.tsx
+++ frontend/src/pages/admin/schools/tab/Subject.tsx
@@ -27,68 +27,39 @@
  *
  */
 import React, { useEffect, useState } from "react";
-import DragAndDrop, { Drag, Drop } from "components/dragAndDrop/DragAndDrop";
 import Tree from "components/tree/Tree";
-import useDatabase from "hooks/useDatabase";
+import Table from "components/table/Table";
+
 type Props = {
   school: any;
 };
 
 const Subject = (props: Props) => {
-  const [subjects, setSubjects] = useState();
-  console.log(props.school);
-  
-  // const database = useDatabase();
-
-  // async function getSelectedSeason(id: string) {
-  //   const result = await database.R({ location: `seasons/${id}` });
-  //   return result;
-  // }
-
-  // useEffect(() => {
-  //   getSelectedSeason().then((res) => {
-  //     setSubjects(res);
-  //   });
-  // }, []);
-
-  function processSubjects(subjectObj: any) {
-    const unique: any = Array.from(
-      new Set(subjectObj.data.map((val: any) => val[0]))
-    );
-    let sub1: any = {};
-    for (let i = 0; i < unique.length; i++) {
-      sub1[unique[i]] = Array.from(
-        new Set(
-          subjectObj.data
-            .filter((val: any) => val[0] === unique[i])
-            .map((val: string[]) => val[1])
-        )
-      );
-    }
-    let sub2: any = {};
-    for (let i = 0; i < unique.length; i++) {
-      for (let index = 0; index < sub1[unique[i]].length; index++) {
-        sub2[`${unique[i]}/${sub1[unique[i]][index]}`] = Array.from(
-          new Set(
-            subjectObj.data
-              .filter(
-                (val: any) =>
-                  val[0] === unique[i] && val[1] === sub1[unique[i]][index]
-              )
-              .map((val: string[]) => val[2])
-          )
-        );
-      }
-    }
-    return [unique, sub1, sub2];
-  }
   return (
     <div style={{ marginTop: "24px" }}>
-      {/* <DragAndDrop>
-        <Drag />
-        <Drop />
-      </DragAndDrop> */}
-      {/* <Tree data={processSubjects(subjects)} /> */}
+      <Table
+        type="string-array"
+        data={props.school?.subjects.data}
+        header={[
+          {
+            text: "ID",
+            key: "",
+            type: "index",
+            width: "48px",
+            align: "center",
+          },
+          {
+            text: "교과",
+            key: 0,
+            type: "string",
+          },
+          {
+            text: "과목",
+            key: 1,
+            type: "string",
+          },
+        ]}
+      />
     </div>
   );
 };
