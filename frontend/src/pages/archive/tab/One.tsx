2bb66dd5fa4d0a245786a25e3d60623400d3dd7e
seedlessapple <luminousseedlessapple@gmail.com>
2022-11-20 11:57:25 +0900

    Update One.tsx

--- frontend/src/pages/archive/tab/One.tsx
+++ frontend/src/pages/archive/tab/One.tsx
@@ -1,24 +1,88 @@
+import { archiveTestData } from "archiveTest";
+import Button from "components/button/Button";
+import Divider from "components/divider/Divider";
 import Autofill from "components/input/Autofill";
-import React from "react";
+import Input from "components/input/Input";
+import Select from "components/select/Select";
+import Table from "components/table/Table";
+import { useAuth } from "contexts/authContext";
+import useDatabase from "hooks/useDatabase";
+import React, { useEffect, useState } from "react";
 import style from "style/pages/archive.module.scss";
 type Props = {
   users: any[];
+  archive?: string;
+  setUserId: React.Dispatch<React.SetStateAction<string>>;
+  userId: string;
 };
 
 const One = (props: Props) => {
+  function archiveData() {
+    return archiveTestData.filter((val) => {
+      return val.label === props.archive;
+    })[0];
+  }
+console.log(props.userId);
 
   return (
-    <div style={{ marginTop: "24px" }}>
+    <>
       <div className={style.search}>
         <div className={style.label}>학생선택</div>
+        <Select
+          options={[{ text: "11학년", value: "11" }]}
+          style={{ borderRadius: "4px", maxWidth: "120px" }}
+        />
         <Autofill
-          appearence="flat"
-          options={props.users.map((val) => {
-            return { text: val.userName, value: val.userId };
-          })}
+          style={{ borderRadius: "4px" }}
+          setState={props.setUserId}
+          defaultValue={props.userId}
+          options={[
+            { text: "", value: "" },
+            ...props.users?.map((val) => {
+              return {
+                value: val.userId,
+                text: `${val.userName} / ${val.userId}`,
+              };
+            }),
+          ]}
+          placeholder={"검색"}
         />
       </div>
-    </div>
+      <Divider />
+      <Button type="ghost" style={{ marginTop: "24px", height: "30px" }}>
+        저장
+      </Button>
+      {archiveData().dataType === "single" ? (
+        <div className={style.content}>
+          <div className={style.labels}>
+            {archiveData().fields?.map((val) => {
+              return <div className={style.label}>{val.label}</div>;
+            })}
+          </div>
+          <div className={style.inputs}>
+            {archiveData().fields?.map((val) => {
+              return <Input placeholder={val.label} />;
+            })}
+          </div>
+        </div>
+      ) : (
+        <div style={{ marginTop: "24px" }}>
+          <Table
+            type="object-array"
+            data={[]}
+            header={
+              archiveData().fields?.map((val, index) => {
+                return {
+                  text: val.label,
+                  key: index,
+                  type: "string",
+                };
+              }) ?? []
+            }
+          />
+        </div>
+      )}
+    </>
   );
 };
 
