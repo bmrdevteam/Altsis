fe344dd9480b7cd3057214c3e2d99561fbf8d9b8
jessie129j@gmail.com <jessie129j@gmail.com>
2022-11-24 11:31:30 +0900

    Update fields to clean in archive model

--- backend/routes/test.js
+++ backend/routes/test.js
@@ -8,18 +8,6 @@ const Season = require("../models/Season");
 const User = require("../models/User");
 
 router.get("/db", async (req, res) => {
-  const _seasons = await Season(req.user.dbName).find({});
-  for (let i = 0; i < _seasons.length; i++) {
-    if (!_seasons[i].period || _.isEmpty(_seasons[i].period)) continue;
-    // console.log(`DEBUG: updating registartions of season[${i}]...`);
-    console.log(`(${_seasons[i]._id},${_seasons[i].year},${_seasons[i].term})`);
-    await Registration(req.user.dbName).update(
-      {
-        season: _seasons[i]._id,
-      },
-      { period: _seasons[i].period }
-    );
-  }
   return res.status(200).json({
     success: true,
     message: "hello world! this is db/test",
