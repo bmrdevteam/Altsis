f27e9bea2dae98ef150274f55ec8f12fcf7aeed7
seedlessapple <luminousseedlessapple@gmail.com>
2022-11-30 13:49:15 +0900

    Update Tree.tsx

--- frontend/src/components/tree/Tree.tsx
+++ frontend/src/components/tree/Tree.tsx
@@ -1,29 +1,52 @@
-import Item from "./Item";
 import style from "./tree.module.scss";
+import Svg from "assets/svg/Svg";
+import { useState } from "react";
 
-type Props = {};
+type Props = {
+  text: string;
+  subItem?: JSX.Element[];
+  onClick?: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+};
 
-const Tree = (props: Props) => {
+const TreeItem = (props: Props) => {
+  const [open, setOpen] = useState<boolean>(false);
   return (
-    <div className={style.tree}>
-      <Item
-        text={"item 1"}
-        subItem={[
-          <Item text={"item 1.1"} subItem={[<Item text={"item 1.1.1"} />]} />,
-          <Item text={"item 1.2"} />,
-          <Item text={"item 1.3"} />,
-        ]}
-      />
-      <Item
-        text={"item 2"}
-        subItem={[
-          <Item text={"item 2.1"} subItem={[<Item text={"item 2.1.1"} />]} />,
-          <Item text={"item 2.2"} />,
-          <Item text={"item 2.3"} />,
-        ]}
-      />
+    <div className={style.item}>
+      <div
+        className={style.content}
+        onClick={(e) => {
+          props.subItem && setOpen((prev) => !prev);
+          props.onClick && props.onClick(e);
+        }}
+      >
+        <span className={style.text}>{props.text}</span>
+        <span style={{ flex: "1 1 0" }}></span>
+        <span className={style.icon}>
+          {props.subItem && (
+            <Svg
+              type={open ? "minus" : "plus"}
+              width={"14px"}
+              height={"14px"}
+            />
+          )}
+        </span>
+      </div>
+      {props.subItem &&
+        open &&
+        props.subItem.map((Element, index) => {
+          return (
+            <div className={style.subItem} key={index}>
+              {Element}
+            </div>
+          );
+        })}
     </div>
   );
 };
 
+const Tree = ({ children }: { children: JSX.Element | JSX.Element[] }) => {
+  return <div className={style.tree}>{children}</div>;
+};
+
+export { TreeItem };
 export default Tree;
