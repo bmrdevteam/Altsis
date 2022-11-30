6378c7dceee639aaf919240090dd40d7037514de
seedlessapple <luminousseedlessapple@gmail.com>
2022-11-30 13:49:01 +0900

    Update archiveTest.ts

--- frontend/src/archiveTest.ts
+++ frontend/src/archiveTest.ts
@@ -1,72 +1,97 @@
-export const archiveTestData = [
+export const archiveTestData:any[] = [
   {
-    label: "인적사항",
+    label: "인적 사항",
     cycle: "unset",
     auth: "teacher / student / teacherID / admin",
-    dataType: "single..?단일 데이터",
+    dataType: "object",
     fields: [
       {
         label: "성명",
-        type: "string",
+        type: "input",
       },
       {
         label: "성별",
-        type: "string",
+        type: "input",
       },
       {
         label: "주민등록번호",
-        type: "string",
+        type: "input",
       },
       {
         label: "주소",
-        type: "string",
+        type: "input",
       },
       {
         label: "성명(부)",
-        type: "string",
+        type: "input",
       },
       {
         label: "성명(모)",
-        type: "string",
+        type: "input",
       },
       {
         label: "생년월일(부)",
-        type: "date",
+        type: "input-date",
       },
       {
         label: "생년월일(모)",
-        type: "date",
+        type: "input-date",
       },
       {
         label: "특기사항",
-        type: "string",
+        type: "input",
       },
     ],
   },
   {
     label: "학적사항",
     cycle: "unset",
-    dataType: "multiple",
+    dataType: "array",
   },
   {
     label: "수상경력",
     cycle: "unset",
-    dataType: "multiple",
+    dataType: "array",
   },
   {
     label: "자격증 및 취득사항",
     cycle: "unset",
-    dataType: "multiple",
+    dataType: "array",
   },
   {
     label: "독서 활동",
-    cycle: "year",
-    dataType: "multiple",
+    cycle: "unset",
+    dataType: "array",
   },
   {
-    label: "봉사 활동",
+    label: "창의적 체험활동 상황",
     cycle: "unset",
-    dataType: "multiple",
+    dataType: "array",
+    fields: [
+      {
+        label: "학년",
+        type: "select",
+        options: ["11학년", "12학년", "10학년"],
+      },
+      {
+        label: "영역",
+        type: "select",
+        options: ["자율활동", "진로활동", "동아리활동", "봉사활동"],
+      },
+      {
+        label: "시간",
+        type: "input-number",
+      },
+      {
+        label: "특기사항",
+        type: "input",
+      },
+    ],
+  },
+  {
+    label: "봉사활동실적",
+    cycle: "unset",
+    dataType: "array",
     fields: [
       {
         label: "학년",
@@ -75,19 +100,19 @@ export const archiveTestData = [
       },
       {
         label: "일자 또는 기간",
-        type: "dateDuration",
+        type: "input",
       },
       {
         label: "장소 또는 주관기관명",
-        type: "string",
+        type: "input",
       },
       {
         label: "활동내용",
-        type: "string",
+        type: "input",
       },
       {
         label: "시간",
-        type: "time",
+        type: "input-number",
       },
     ],
   },
