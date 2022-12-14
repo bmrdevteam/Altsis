ce88a5bf9227d55d6a623ee3be318fe58bd98e51
jessie129j@gmail.com <jessie129j@gmail.com>
2022-12-14 14:55:02 +0900

    Use RequireAuth to authenticate Owner pages

--- frontend/src/pages/owner/Index.tsx
+++ frontend/src/pages/owner/Index.tsx
@@ -27,37 +27,17 @@
  *
  */
 
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useAuth } from "contexts/authContext";
-
 import Navbar from "layout/navbar/Navbar";
 import style from "style/pages/owner/academy.module.scss";
 
 type Props = {};
 
 const Academies = (props: Props) => {
-  const navigate = useNavigate();
-  const { currentUser } = useAuth();
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  useEffect(() => {
-    if (currentUser.auth !== "owner") {
-      alert("접근 권한이 없습니다.");
-      navigate("/");
-    } else {
-      setIsAuthenticated(true);
-    }
-  }, [currentUser]);
-
-  return isAuthenticated ? (
+  return (
     <>
       <Navbar />
-      asdfasd
       <div className={style.section}></div>
     </>
-  ) : (
-    <></>
   );
 };
 
