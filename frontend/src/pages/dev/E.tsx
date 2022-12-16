f032ef19b2c12d2d6a3cb23119254c99e83ca343
seedlessapple <luminousseedlessapple@gmail.com>
2022-12-16 11:51:29 +0900

    Update E.tsx

--- frontend/src/pages/dev/E.tsx
+++ frontend/src/pages/dev/E.tsx
@@ -1,14 +1,23 @@
 import Tree from "components/tree/Tree";
-
+import useApi from "hooks/useApi";
+import { useEffect, useState } from "react";
 
 type Props = {};
 
 const E = (props: Props) => {
-  return (
-    <div>
-      {/* <Tree/> */}
-    </div>
-  );
+  const { UserApi } = useApi();
+
+  const [user, setUser] = useState();
+  useEffect(() => {
+    UserApi.RMySelf().then((res) => {
+      setUser(res);
+    });
+    
+  }, []);
+  console.log(user);
+  
+
+  return <div></div>;
 };
 
 export default E;
