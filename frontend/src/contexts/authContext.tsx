b9259b83bf3136fffcd0c59887c62970532f87b3
seedlessapple <luminousseedlessapple@gmail.com>
2022-12-19 10:00:30 +0900

    Update authContext.tsx

--- frontend/src/contexts/authContext.tsx
+++ frontend/src/contexts/authContext.tsx
@@ -30,10 +30,6 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
 
   const [currentSchool, setCurrentSchool] = useState<any>();
   const [registrations, setRegistration] = useState<any>([]);
-  console.log(
-    "🚀 ~ file: authContext.tsx ~ line 25 ~ AuthProvider ~ registrations",
-    registrations
-  );
   const [currentRegistration, setCurrentRegistration] = useState<any>();
   const [currentSeason, setCurrentSeason] = useState<any>();
   const [currentNotifications, setCurrentNotifications] = useState<any>([]);
@@ -53,7 +49,9 @@ export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
       if (res.registrations) {
         setRegistration(res.registrations);
         setCurrentRegistration(res.registrations[0]);
-        changeCurrentSeason(res.registrations[0]);
+        SeasonApi.RGetSeasonById(res.registrations[0].season).then((res) => {
+          setCurrentSeason(res);
+        });
       }
     });
   }
