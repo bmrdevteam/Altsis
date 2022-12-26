24780d790b8fe0c8e4c1dda8a7d2b3cf6b13b3d8
jessie129j@gmail.com <jessie129j@gmail.com>
2022-12-27 00:02:07 +0900

    Update Form.tsx(admin/schools/form/evaluation)

--- frontend/src/pages/admin/schools/tab/Form.tsx
+++ frontend/src/pages/admin/schools/tab/Form.tsx
@@ -27,7 +27,7 @@
  *
  */
 import { useEffect, useState } from "react";
-
+import { useAuth } from "contexts/authContext";
 import useDatabase from "hooks/useDatabase";
 
 // components
@@ -37,17 +37,24 @@ import Table from "components/tableV2/Table";
 
 import style from "style/pages/admin/schools.module.scss";
 
-type Props = {
-  schoolData: any;
-};
+type Props = {};
 
 const Form = (props: Props) => {
   const database = useDatabase();
-  const [selectFormPopupActive, setSelectFormPopupActive] =
-    useState<boolean>(false);
-  const [selectFormType, setSelectFormType] = useState<string>();
+
+  const { currentSchool, setCurrentSchool } = useAuth();
+
   const [forms, setForms] = useState<any>();
-  const [season, setSeason] = useState<any>();
+  const [formEvaluation, setFormEvaluation] = useState<any[]>(
+    currentSchool.formEvaluation || []
+  );
+
+  const [formTimetablePopupActive, setFormTimetablePopupActive] =
+    useState<boolean>(false);
+  const [formSyllabusPopupActive, setFormSyllabusPopupActive] =
+    useState<boolean>(false);
+  const [formEvaluationPopupActive, setFormEvaluationPopupActive] =
+    useState<boolean>(false);
 
   async function getForms() {
     const { forms: result } = await database.R({ location: "forms" });
@@ -57,41 +64,32 @@ const Form = (props: Props) => {
     const result = await database.R({ location: `forms/${id}` });
     return result;
   }
-  async function getSeason() {
-    const result = await database.R({
-      location: `schools/${props.schoolData._id}`,
+
+  async function updateFormTimetable(data: any) {
+    const { formTimetable: result } = await database.U({
+      location: `schools/${currentSchool.school}/form/timetable`,
+      data: { new: data },
+    });
+    return result;
+  }
+  async function updateFormSyllabus(data: any) {
+    const { formSyllabus: result } = await database.U({
+      location: `schools/${currentSchool.school}/form/syllabus`,
+      data: { new: data },
+    });
+    return result;
+  }
+  async function updateFormEvaluation(data: any) {
+    const { formEvaluation: result } = await database.U({
+      location: `schools/${currentSchool.school}/form/evaluation`,
+      data: { new: data },
     });
     return result;
   }
 
-  async function updateSeasonFormTimetable(data: any) {
-    const result = await database.U({
-      location: `schools/${props.schoolData._id}/form/timetable`,
-      data: { new: data },
-    });
-    return result;
-  }
-  async function updateSeasonFormSyllabus(data: any) {
-    const result = await database.U({
-      location: `schools/${props.schoolData._id}/form/syllabus`,
-      data: { new: data },
-    });
-    return result;
-  }
-  async function updateSeasonFormEvaluation(data: any) {
-    const result = await database.U({
-      location: `schools/${props.schoolData._id}/form/evaluation`,
-      data: { new: data },
-    });
-    return result;
-  }
   useEffect(() => {
     getForms().then((res) => {
-      console.log(res);
       setForms(res);
-    });
-    getSeason().then((res) => {
-      setSeason(res);
     });
   }, []);
 
@@ -105,11 +103,10 @@ const Form = (props: Props) => {
             style={{ marginTop: "12px" }}
             type="ghost"
             onClick={() => {
-              setSelectFormType("timetable");
-              setSelectFormPopupActive(true);
+              setFormTimetablePopupActive(true);
             }}
           >
-            {season?.formTimetable?.title ?? "선택"}
+            {currentSchool.formTimetable?.title ?? "선택"}
           </Button>
         </div>
         <div className={style.item}>
@@ -118,11 +115,10 @@ const Form = (props: Props) => {
             style={{ marginTop: "12px" }}
             type="ghost"
             onClick={() => {
-              setSelectFormType("syllabus");
-              setSelectFormPopupActive(true);
+              setFormSyllabusPopupActive(true);
             }}
           >
-            {season?.formSyllabus?.title ?? "선택"}
+            {currentSchool.formSyllabus?.title ?? "선택"}
           </Button>
         </div>
         <div className={style.item}>
@@ -131,15 +127,14 @@ const Form = (props: Props) => {
             style={{ marginTop: "12px" }}
             type="ghost"
             onClick={() => {
-              setSelectFormType("evaluation");
-              setSelectFormPopupActive(true);
+              setFormEvaluationPopupActive(true);
             }}
           >
-            {season?.formEvaluation?.title ?? "선택"}
+            설정
           </Button>
         </div>
       </div>
-      {selectFormPopupActive && (
+      {/* {selectFormPopupActive && (
         <Popup
           style={{ borderRadius: "4px", maxWidth: "600px", width: "100%" }}
           title={`${
@@ -174,24 +169,24 @@ const Form = (props: Props) => {
                   getForm(id).then((res) => {
                     switch (selectFormType) {
                       case "timetable":
-                        updateSeasonFormTimetable(res).then(() => {
-                          getSeason().then((res) => {
+                        updateFormTimetable(res).then(() => {
+                          getSchool().then((res) => {
                             setSeason(res);
                           });
                         });
                         setSelectFormPopupActive(false);
                         break;
                       case "syllabus":
-                        updateSeasonFormSyllabus(res).then(() => {
-                          getSeason().then((res) => {
+                        updateFormSyllabus(res).then(() => {
+                          getSchool().then((res) => {
                             setSeason(res);
                           });
                         });
                         setSelectFormPopupActive(false);
                         break;
                       case "evaluation":
-                        updateSeasonFormEvaluation(res).then(() => {
-                          getSeason().then((res) => {
+                        updateFormEvaluation(res).then(() => {
+                          getSchool().then((res) => {
                             setSeason(res);
                           });
                         });
@@ -210,6 +205,220 @@ const Form = (props: Props) => {
                   padding: "4px",
                   round: true,
                 },
+              },
+            ]}
+          />
+        </Popup>
+      )} */}
+      {formTimetablePopupActive && (
+        <Popup
+          style={{ borderRadius: "4px", maxWidth: "600px", width: "100%" }}
+          title={`시간표 양식 선택`}
+          setState={setFormTimetablePopupActive}
+          closeBtn
+        >
+          <Table
+            type="object-array"
+            data={forms?.filter((val: any) => val.type === "timetable")}
+            header={[
+              {
+                text: "No",
+                type: "text",
+                key: "tableRowIndex",
+                width: "48px",
+                textAlign: "center",
+              },
+              { text: "제목", key: "title", type: "text" },
+              {
+                text: "선택",
+                key: "select",
+                type: "button",
+                onClick: (e: any) => {
+                  const id = e._id;
+
+                  getForm(id).then((res) => {
+                    updateFormTimetable(res).then(() => {
+                      console.log("res is ", res);
+                      setCurrentSchool({
+                        ...currentSchool,
+                        formTimetable: res,
+                      });
+                      setFormTimetablePopupActive(false);
+                    });
+                  });
+                },
+                width: "80px",
+                textAlign: "center",
+                btnStyle: {
+                  border: true,
+                  color: "black",
+                  padding: "4px",
+                  round: true,
+                },
+              },
+            ]}
+          />
+        </Popup>
+      )}
+      {formSyllabusPopupActive && (
+        <Popup
+          style={{ borderRadius: "4px", maxWidth: "600px", width: "100%" }}
+          title={`강의계획서 양식 선택`}
+          setState={setFormSyllabusPopupActive}
+          closeBtn
+        >
+          <Table
+            type="object-array"
+            data={forms?.filter((val: any) => val.type === "syllabus")}
+            header={[
+              {
+                text: "No",
+                type: "text",
+                key: "tableRowIndex",
+                width: "48px",
+                textAlign: "center",
+              },
+              { text: "제목", key: "title", type: "text" },
+              {
+                text: "선택",
+                key: "select",
+                type: "button",
+                onClick: (e: any) => {
+                  const id = e._id;
+
+                  getForm(id).then((res) => {
+                    updateFormSyllabus(res).then(() => {
+                      console.log("res is ", res);
+                      setCurrentSchool({
+                        ...currentSchool,
+                        formSyllabus: res,
+                      });
+                      setFormSyllabusPopupActive(false);
+                    });
+                  });
+                },
+                width: "80px",
+                textAlign: "center",
+                btnStyle: {
+                  border: true,
+                  color: "black",
+                  padding: "4px",
+                  round: true,
+                },
+              },
+            ]}
+          />
+        </Popup>
+      )}
+      {formEvaluationPopupActive && (
+        <Popup
+          style={{ borderRadius: "4px", maxWidth: "600px", width: "100%" }}
+          title={`평가 양식 설정`}
+          setState={setFormEvaluationPopupActive}
+          closeBtn
+          contentScroll
+          footer={
+            <Button
+              type={"ghost"}
+              onClick={() => {
+                const _formEvaluation = formEvaluation?.map((elem: any) => {
+                  elem.auth = {
+                    edit: {
+                      teacher:
+                        elem.authOption === "editByTeacher" ||
+                        elem.authOption === "editByTeacherAndStudentCanView",
+                      student: elem.authOption === "editByStudent",
+                    },
+                    view: {
+                      teacher: true,
+                      student:
+                        elem.authOption === "editByStudent" ||
+                        elem.authOption === "editByTeacherAndStudentCanView",
+                    },
+                  };
+                  return elem;
+                });
+
+                updateFormEvaluation(_formEvaluation).then((res: any) => {
+                  console.log("res is ", res);
+                  setCurrentSchool({
+                    ...currentSchool,
+                    formEvaluation: res,
+                  });
+                  setFormEvaluationPopupActive(false);
+                });
+              }}
+            >
+              저장
+            </Button>
+          }
+        >
+          <Table
+            type="object-array"
+            data={currentSchool.formEvaluation ?? []}
+            onChange={(e: any[]) => {
+              setFormEvaluation(
+                e.filter(
+                  (elem: any) => elem.label && elem.type && elem.authOption
+                )
+              );
+            }}
+            header={[
+              {
+                type: "text",
+                text: "평가 항목",
+                key: "label",
+              },
+              {
+                text: "유형",
+                key: "type",
+                fontSize: "12px",
+                fontWeight: "600",
+                type: "status",
+                status: {
+                  input: {
+                    text: "텍스트",
+                    color: "#B33F00",
+                  },
+                  "input-number": {
+                    text: "숫자",
+                    color: "#00B3AD",
+                  },
+                },
+                width: "80px",
+                textAlign: "center",
+              },
+              {
+                text: "평가자",
+                key: "authOption",
+                fontSize: "12px",
+                fontWeight: "600",
+                type: "status",
+                status: {
+                  editByTeacher: {
+                    text: "선생님",
+                    color: "red",
+                  },
+                  editByStudent: {
+                    text: "학생",
+                    color: "blue",
+                  },
+                  editByTeacherAndStudentCanView: {
+                    text: "선생님(학생 조회 가능)",
+                    color: "purple",
+                  },
+                },
+                width: "180px",
+                textAlign: "center",
+              },
+
+              {
+                text: "수정",
+                type: "rowEdit",
+                fontSize: "12px",
+                fontWeight: "600",
+                textAlign: "center",
+                width: "80px",
               },
             ]}
           />
