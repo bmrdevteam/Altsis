6ae5405503e368f82e21072c933f58462ff013d3
Yeonwu Oh <yeonwu319@gmail.com>
2023-01-05 22:24:07 +0900

    Add schedule tab

---
+++ frontend/src/pages/userSearchResult/tab/ScheduleTab.tsx
@@ -0,0 +1,75 @@
+import Schedule from "components/schedule/Schedule";
+import { useAuth } from "contexts/authContext";
+import useApi from "hooks/useApi";
+import { useEffect, useState } from "react";
+
+type Props = {
+	user: any
+}
+
+const ScheduleTab = (props: Props) => {
+	const {user} = props;
+	
+	const {EnrollmentApi} = useApi();
+	const {currentSeason} = useAuth();
+
+	const [enrollments, setEnrollments] = useState<any>();
+
+	function enrollmentsToEvents(data: any[]) {
+    if (data) {
+      let result: any[] = [];
+      for (let i = 0; i < data.length; i++) {
+        const element = data[i];
+
+        if (element?.time.length <= 1) {
+          result.push({
+            id: element._id + JSON.stringify(element?.time[0]),
+            type: "course",
+            classroom: element.classroom,
+            title: element.classTitle,
+            startTime: element?.time[0].start,
+            endTime: element?.time[0].end,
+            day: element?.time[0].day,
+          });
+        } else {
+          for (let ii = 0; ii < element?.time.length; ii++) {
+            const time = element?.time[ii];
+            result.push({
+              id: element._id + JSON.stringify(time),
+              title: element.classTitle,
+              type: "course",
+              classroom: element.classroom,
+              startTime: time.start,
+              endTime: time.end,
+              day: time.day,
+            });
+          }
+        }
+      }
+      return result;
+    }
+  }
+
+	useEffect(() => {
+    if (currentSeason && user) {
+      EnrollmentApi.REnrolllments({
+        season: currentSeason._id,
+        studentId: user.userId,
+      })
+      .then((res) => {
+        setEnrollments(res);
+      })
+      .catch(() => {}); 
+    }
+  }, [currentSeason, user]);
+
+	return<Schedule
+		dayArray={["월", "화", "수", "목", "금"]}
+		defaultEvents={enrollmentsToEvents(enrollments)}
+		title={`${currentSeason?.year ?? ""} ${
+			currentSeason?.term ?? ""
+		} 일정`}
+	/>
+}
+
+export default ScheduleTab;
