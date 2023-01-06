bf4aefad5d2093ebad1d9294cfee98da6b6ec75f
jessie129j@gmail.com <jessie129j@gmail.com>
2023-01-06 16:38:59 +0900

    #22 integrate course pages into tab

---
+++ frontend/src/pages/courses/tab/MentoringCourseList.tsx
@@ -0,0 +1,139 @@
+/**
+ * @file Mentoring Page
+ *
+ * @author jessie129j <jessie129j@gmail.com>
+ *
+ * -------------------------------------------------------
+ *
+ * IN PRODUCTION
+ *
+ * -------------------------------------------------------
+ *
+ * IN MAINTENANCE
+ *
+ * -------------------------------------------------------
+ *
+ * IN DEVELOPMENT
+ *
+ * -------------------------------------------------------
+ *
+ * DEPRECATED
+ *
+ * -------------------------------------------------------
+ *
+ * NOTES
+ *
+ * @version 1.0
+ *
+ */
+
+import { useNavigate } from "react-router-dom";
+import style from "style/pages/enrollment.module.scss";
+
+// components
+import Table from "components/tableV2/Table";
+
+type Props = { courseList: any[]; subjectLabelHeaderList: any[] };
+
+const CoursesMentoring = (props: Props) => {
+  const navigate = useNavigate();
+
+  const subjectHeaderList = [
+    {
+      text: "수업명",
+      key: "classTitle",
+      type: "text",
+      textAlign: "center",
+    },
+
+    {
+      text: "시간",
+      key: "timeText",
+      type: "string",
+      textAlign: "center",
+    },
+    {
+      text: "강의실",
+      key: "classroom",
+      type: "string",
+      textAlign: "center",
+    },
+
+    {
+      text: "학점",
+      key: "point",
+      type: "string",
+      textAlign: "center",
+    },
+    {
+      text: "수강/정원",
+      key: "count_limit",
+      type: "string",
+      textAlign: "center",
+    },
+    {
+      text: "개설자",
+      key: "userName",
+      type: "string",
+      textAlign: "center",
+    },
+    {
+      text: "멘토",
+      key: "mentorText",
+      type: "string",
+      textAlign: "center",
+    },
+    {
+      text: "상태",
+      key: "confirmed",
+      width: "72px",
+      textAlign: "center",
+      type: "status",
+      status: {
+        false: { text: "미승인", color: "red" },
+        true: { text: "승인됨", color: "green" },
+      },
+    },
+    {
+      text: "자세히",
+      key: "detail",
+      type: "button",
+      onClick: (e: any) => {
+        navigate(`/courses/mentoring/${e._id}`, {
+          replace: true,
+        });
+      },
+      width: "72px",
+      textAlign: "center",
+      btnStyle: {
+        border: true,
+        color: "black",
+        padding: "4px",
+        round: true,
+      },
+    },
+  ];
+
+  return (
+    <div className={style.section}>
+      <Table
+        control
+        type="object-array"
+        data={props.courseList}
+        header={[
+          {
+            text: "No",
+            type: "text",
+            key: "tableRowIndex",
+            width: "48px",
+            textAlign: "center",
+          },
+          ...props.subjectLabelHeaderList,
+          ...subjectHeaderList,
+        ]}
+      />
+    </div>
+  );
+};
+
+export default CoursesMentoring;
---
+++ frontend/src/pages/courses/view/Created.tsx
@@ -0,0 +1,289 @@
+/**
+ * @file Courses Pid Page
+ *
+ * more info on selected courses
+ *
+ * @author jessie129j <jessie129j@gmail.com>
+ *
+ * -------------------------------------------------------
+ *
+ * IN PRODUCTION
+ *
+ * -------------------------------------------------------
+ *
+ * IN MAINTENANCE
+ *
+ * -------------------------------------------------------
+ *
+ * IN DEVELOPMENT
+ *
+ * -------------------------------------------------------
+ *
+ * DEPRECATED
+ *
+ * -------------------------------------------------------
+ *
+ * NOTES
+ *
+ * @version 1.0
+ *
+ */
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import useApi from "hooks/useApi";
+import { useAuth } from "contexts/authContext";
+import style from "style/pages/courses/course.module.scss";
+
+import Navbar from "layout/navbar/Navbar";
+
+import _ from "lodash";
+import useDatabase from "hooks/useDatabase";
+import EditorParser from "editor/EditorParser";
+import Divider from "components/divider/Divider";
+import Button from "components/button/Button";
+
+import Table from "components/tableV2/Table";
+import Popup from "components/popup/Popup";
+
+type Props = {};
+
+const CoursePid = (props: Props) => {
+  const { pid } = useParams<"pid">();
+  const { SyllabusApi } = useApi();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  const [courseData, setCourseData] = useState<any>();
+
+  const navigate = useNavigate();
+  const { currentSeason, currentUser } = useAuth();
+  const database = useDatabase();
+
+  const [confirmStatusPopupActive, setConfirmStatusPopupActive] =
+    useState<boolean>(false);
+
+  const [confirmed, setConfirmed] = useState<boolean>(true);
+
+  const categories = () => {
+    return (
+      <>
+        {currentSeason?.subjects?.label && (
+          <div className={style.category}>
+            {_.join(currentSeason?.subjects.label, "/")}:{" "}
+            {_.join(courseData.subject, "/")}
+          </div>
+        )}
+        <div className={style.category}>
+          강의실: {courseData.classroom || "없음"}
+        </div>
+        <div className={style.category}>
+          시간:{" "}
+          {_.join(
+            courseData?.time.map((timeBlock: any) => timeBlock.label),
+            ", "
+          )}
+        </div>
+        <div className={style.category}>학점: {courseData.point}</div>
+        <div className={style.category}>수강정원: {courseData.limit}</div>
+        <div className={style.category}>개설자: {courseData.userName}</div>
+        <div className={style.category}>
+          멘토:{" "}
+          {_.join(
+            courseData.teachers?.map((teacher: any) => teacher.userName),
+            ", "
+          )}
+        </div>
+        <div
+          className={style.category}
+          onClick={() => {
+            setConfirmStatusPopupActive(true);
+          }}
+        >
+          상태: {confirmed ? "승인됨" : "미승인"}
+        </div>
+      </>
+    );
+  };
+
+  async function deleteCourse() {
+    if (window.confirm("정말 삭제하시겠습니까?") === true) {
+      const result = database.D({
+        location: `syllabuses/${courseData._id}`,
+      });
+      return result;
+    } else {
+      return false;
+    }
+  }
+
+  useEffect(() => {
+    if (isLoading) {
+      SyllabusApi.RSyllabus(pid)
+        .then((result) => {
+          setCourseData(result);
+
+          if (
+            result.season !== currentSeason._id ||
+            result.userId !== currentUser.userId
+          ) {
+            navigate("/courses#개설한%20수업%20목록", { replace: true });
+          }
+
+          // is this syllabus fully confirmed?
+          for (let teacher of result.teachers) {
+            if (!teacher.confirmed) {
+              setConfirmed(false);
+              break;
+            }
+          }
+
+          setIsLoading(false);
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+          navigate("/courses");
+        });
+    }
+    return () => {};
+  }, [isLoading]);
+
+  const ClassInfo = () => {
+    return (
+      <EditorParser
+        auth="view"
+        defaultValues={courseData.info}
+        data={currentSeason?.formSyllabus}
+      />
+    );
+  };
+
+  return !isLoading ? (
+    <>
+      <Navbar />
+      <div className={style.section}>
+        <div
+          style={{
+            fontSize: "12px",
+            fontWeight: 500,
+            marginBottom: "18px",
+            display: "flex",
+            color: "var(--accent-1)",
+          }}
+        >
+          <div style={{ wordBreak: "keep-all" }}>
+            <span>&nbsp;/&nbsp;</span>
+            <span
+              style={{ cursor: "pointer" }}
+              onClick={() => {
+                navigate("/courses#개설한%20수업%20목록", { replace: true });
+              }}
+            >
+              {`개설한 수업 목록 / ${pid}`}
+            </span>
+          </div>
+        </div>
+        <>
+          <div className={style.title}>{courseData.classTitle}</div>
+          <div className={style.categories_container}>
+            <div className={style.categories}>{categories()}</div>
+          </div>
+          <Divider />
+          <ClassInfo />
+          <div style={{ height: "24px" }}></div>
+          <Divider />
+
+          {!confirmed && (
+            <>
+              <Button
+                type={"ghost"}
+                style={{
+                  borderRadius: "4px",
+                  height: "32px",
+                  boxShadow: "rgba(0, 0, 0, 0.1) 0px 1px 2px 0px",
+                  marginTop: "12px",
+                }}
+                onClick={() => {
+                  alert("clicked");
+                  navigate(`/courses/edit/${pid}`, { replace: true });
+                }}
+              >
+                수정
+              </Button>
+              <Button
+                type={"ghost"}
+                style={{
+                  borderRadius: "4px",
+                  height: "32px",
+                  boxShadow: "rgba(0, 0, 0, 0.1) 0px 1px 2px 0px",
+                  marginTop: "12px",
+                }}
+                onClick={() => {
+                  deleteCourse()
+                    .then((res) => {
+                      alert("success");
+                      navigate("/courses");
+                    })
+                    .catch((err) => {
+                      alert(err.response.body.message);
+                    });
+                }}
+              >
+                삭제
+              </Button>
+            </>
+          )}
+          <div style={{ height: "24px" }}></div>
+
+          {confirmStatusPopupActive && (
+            <Popup
+              setState={setConfirmStatusPopupActive}
+              title="승인 상태"
+              closeBtn
+            >
+              <Table
+                type="object-array"
+                data={courseData.teachers}
+                header={[
+                  {
+                    text: "No",
+                    type: "text",
+                    key: "tableRowIndex",
+                    width: "48px",
+                    textAlign: "center",
+                  },
+                  {
+                    text: "멘토 ID",
+                    key: "userId",
+                    type: "text",
+                    textAlign: "center",
+                  },
+                  {
+                    text: "멘토 이름",
+                    key: "userName",
+                    type: "text",
+                    textAlign: "center",
+                  },
+
+                  {
+                    text: "상태",
+                    key: "confirmed",
+                    width: "120px",
+                    textAlign: "center",
+                    type: "status",
+                    status: {
+                      false: { text: "미승인", color: "red" },
+                      true: { text: "승인됨", color: "green" },
+                    },
+                  },
+                ]}
+              />
+            </Popup>
+          )}
+        </>
+      </div>
+    </>
+  ) : (
+    <>로딩중</>
+  );
+};
+
+export default CoursePid;
