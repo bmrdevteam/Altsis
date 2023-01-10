4735b0d7e14ffe4202b09a2d8689bc7b35ca6e72
jessie129j@gmail.com <jessie129j@gmail.com>
2023-01-10 11:33:33 +0900

    [FIX] #35 Update Subject.tsx (admin>seasons)

--- backend/models/Season.js
+++ backend/models/Season.js
@@ -25,7 +25,7 @@ const seasonSchema = mongoose.Schema(
       required: true,
     },
     classrooms: [String],
-    subjects: subjectSchema,
+    subjects: { type: subjectSchema, default: { label: [], data: [] } },
     year: {
       type: String,
       required: true,
