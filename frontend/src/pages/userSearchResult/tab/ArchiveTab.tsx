4097965f3aea018c3cd708de1ccea0bd9cc59f10
Yeonwu Oh <yeonwu319@gmail.com>
2023-01-14 17:44:08 +0900

    Add app, archive, docs tab dummy

---
+++ frontend/src/pages/userSearchResult/tab/ArchiveTab.tsx
@@ -0,0 +1,9 @@
+type Props = {
+	user: any
+}
+
+const ArchiveTab = (props: Props) => {
+	return <div>기록</div>
+}
+
+export default ArchiveTab;
---
+++ frontend/src/pages/userSearchResult/tab/DocsTab.tsx
@@ -0,0 +1,9 @@
+type Props = {
+	user: any
+}
+
+const DocsTab = (props: Props) => {
+	return <div>문서</div>
+};
+
+export default DocsTab;
