f3f6ca1f7af843be8626fd4f9cbfd07280f27180
jessie129j@gmail.com <jessie129j@gmail.com>
2023-03-20 15:34:10 +0900

    fix: Solve undefined archiveForm error #60

--- frontend/src/pages/archive/Index.tsx
+++ frontend/src/pages/archive/Index.tsx
@@ -13,8 +13,11 @@ const Archive = (props: Props) => {
   // const database = useDatabase();
   const navigate = useNavigate();
   const { currentSchool } = useAuth();
+
   useEffect(() => {
-    navigate(currentSchool.formArchive[0].label);
+    if (currentSchool?.formArchive.length !== 0) {
+      navigate(currentSchool.formArchive[0].label);
+    }
   }, []);
 
   // const [loading, setLoading] = useState<boolean>(true);
@@ -125,6 +128,31 @@ const Archive = (props: Props) => {
 
   return (
     <>
+      {currentSchool?.formArchive.length === 0 && (
+        <>
+          <Navbar />
+          <div className={style.section}>
+            <div style={{ display: "flex", gap: "24px" }}>
+              <div style={{ flex: "1 1 0" }}>
+                <div className={style.title}>기록</div>
+                <div className={style.description}>
+                  {`등록된 양식이 없습니다.`}
+                  <br />
+                  <a
+                    href={"/admin/schools/" + currentSchool._id + "#기록"}
+                    title={"/admin/schools/" + currentSchool._id + "#기록"}
+                    style={{ textDecoration: "underline" }}
+                  >
+                    {"관리자 > 학교 > 기록"}
+                  </a>
+                  {`에서 양식을 등록해주세요.`}
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+
       {/* <Navbar />
       {!loading ? (
         <div className={style.section}>
