c0ddf49f8b207b7ec85e4acda0e9ced50d0b5057
jessie129j@gmail.com <jessie129j@gmail.com>
2023-04-05 14:18:18 +0900

    feat: Update Created page #147

--- frontend/src/pages/courses/view/Created.tsx
+++ frontend/src/pages/courses/view/Created.tsx
@@ -37,7 +37,6 @@ import style from "style/pages/courses/course.module.scss";
 import Navbar from "layout/navbar/Navbar";
 
 import _ from "lodash";
-import useDatabase from "hooks/useDatabase";
 import EditorParser from "editor/EditorParser";
 import Divider from "components/divider/Divider";
 import Button from "components/button/Button";
@@ -51,19 +50,15 @@ type Props = {};
 
 const CoursePid = (props: Props) => {
   const { pid } = useParams<"pid">();
-  const { SyllabusApi } = useApi();
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-
-  const [courseData, setCourseData] = useState<any>();
-
-  const navigate = useNavigate();
   const { currentSeason, currentUser } = useAuth();
-  const database = useDatabase();
+  const { SyllabusApi, EnrollmentApi } = useApi();
+  const navigate = useNavigate();
+
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [syllabus, setSyllabus] = useState<any>();
 
   const [confirmStatusPopupActive, setConfirmStatusPopupActive] =
     useState<boolean>(false);
-
-  const [confirmed, setConfirmed] = useState<boolean>(true);
   const [confirmedStatus, setConfirmedStatus] =
     useState<string>("notConfirmed");
 
@@ -73,26 +68,26 @@ const CoursePid = (props: Props) => {
         {currentSeason?.subjects?.label && (
           <div className={style.category}>
             {_.join(currentSeason?.subjects.label, "/")}:{" "}
-            {_.join(courseData.subject, "/")}
+            {_.join(syllabus.subject, "/")}
           </div>
         )}
         <div className={style.category}>
-          강의실: {courseData.classroom || "없음"}
+          강의실: {syllabus.classroom || "없음"}
         </div>
         <div className={style.category}>
           시간:{" "}
           {_.join(
-            courseData?.time.map((timeBlock: any) => timeBlock.label),
+            syllabus?.time.map((timeBlock: any) => timeBlock.label),
             ", "
           )}
         </div>
-        <div className={style.category}>학점: {courseData.point}</div>
-        <div className={style.category}>수강정원: {courseData.limit}</div>
-        <div className={style.category}>개설자: {courseData.userName}</div>
+        <div className={style.category}>학점: {syllabus.point}</div>
+        <div className={style.category}>수강정원: {syllabus.limit}</div>
+        <div className={style.category}>개설자: {syllabus.userName}</div>
         <div className={style.category}>
           멘토:{" "}
           {_.join(
-            courseData.teachers?.map((teacher: any) => teacher.userName),
+            syllabus.teachers?.map((teacher: any) => teacher.userName),
             ", "
           )}
         </div>
@@ -113,49 +108,35 @@ const CoursePid = (props: Props) => {
     );
   };
 
-  async function deleteCourse() {
-    if (window.confirm("정말 삭제하시겠습니까?") === true) {
-      const result = database.D({
-        location: `syllabuses/${courseData._id}`,
-      });
-      return result;
-    } else {
-      return false;
-    }
-  }
-
   useEffect(() => {
     if (isLoading) {
       SyllabusApi.RSyllabus(pid)
         .then((result) => {
-          setCourseData(result);
+          setSyllabus(result);
 
           if (
             result.season !== currentSeason._id ||
-            result.userId !== currentUser.userId
+            result.user !== currentUser._id
           ) {
             navigate("/courses#개설%20수업", { replace: true });
           }
 
           // is this syllabus fully confirmed?
-          for (let teacher of result.teachers) {
-            if (!teacher.confirmed) {
-              setConfirmed(false);
-              break;
+          let confirmedCnt = 0;
+          for (let teacher of result?.teachers) {
+            if (teacher.confirmed) {
+              confirmedCnt += 1;
             }
           }
-
-          const confirmedCnt = _.filter(result.teachers, {
-            confirmed: true,
-          }).length;
           setConfirmedStatus(
             confirmedCnt === 0
               ? "notConfirmed"
-              : confirmedCnt === result.teachers.length
+              : confirmedCnt === result?.teachers.length
               ? "fullyConfirmed"
               : "semiConfirmed"
           );
-
+        })
+        .then(() => {
           setIsLoading(false);
         })
         .catch((err) => {
@@ -166,22 +147,12 @@ const CoursePid = (props: Props) => {
     return () => {};
   }, [isLoading]);
 
-  const ClassInfo = () => {
-    return (
-      <EditorParser
-        type="syllabus"
-        auth="view"
-        defaultValues={courseData.info}
-        data={currentSeason?.formSyllabus}
-      />
-    );
-  };
-
   return !isLoading ? (
     <>
       <Navbar />
       <div className={style.section}>
         <div
+          className="syllabus-header"
           style={{
             fontSize: "12px",
             fontWeight: 500,
@@ -202,11 +173,11 @@ const CoursePid = (props: Props) => {
               {`개설한 수업 목록 / ${pid}`}
             </span>
           </div>
-          {_.find(courseData.teachers, { _id: currentUser._id }) && (
+          {_.find(syllabus.teachers, { _id: currentUser._id }) && (
             <div
               className={style.icon}
               onClick={(e: any) => {
-                navigate(`/courses/mentoring/${courseData._id}`, {
+                navigate(`/courses/mentoring/${syllabus._id}`, {
                   replace: true,
                 });
               }}
@@ -217,104 +188,151 @@ const CoursePid = (props: Props) => {
             </div>
           )}
         </div>
-        <>
-          <div className={style.title}>{courseData.classTitle}</div>
-          <div className={style.categories_container}>
-            <div className={style.categories}>{categories()}</div>
-          </div>
-          <Divider />
-          <ClassInfo />
-          <div style={{ height: "24px" }}></div>
-          <Divider />
 
-          {!confirmed && (
-            <>
-              <Button
-                type={"ghost"}
-                style={{
-                  borderRadius: "4px",
-                  height: "32px",
-                  boxShadow: "rgba(0, 0, 0, 0.1) 0px 1px 2px 0px",
-                  marginTop: "12px",
-                }}
-                onClick={() => {
-                  navigate(`/courses/edit/${pid}`, { replace: true });
-                }}
-              >
-                수정
-              </Button>
-              <Button
-                type={"ghost"}
-                style={{
-                  borderRadius: "4px",
-                  height: "32px",
-                  boxShadow: "rgba(0, 0, 0, 0.1) 0px 1px 2px 0px",
-                  marginTop: "12px",
-                }}
-                onClick={() => {
-                  deleteCourse()
-                    .then((res) => {
+        <div className={style.title}>{syllabus.classTitle}</div>
+        <div className={style.categories_container}>
+          <div className={style.categories}>{categories()}</div>
+        </div>
+        <Divider />
+        <EditorParser
+          type="syllabus"
+          auth="view"
+          defaultValues={syllabus.info}
+          data={currentSeason?.formSyllabus}
+        />
+        <div style={{ height: "24px" }}></div>
+        <Divider />
+
+        {confirmedStatus === "notConfirmed" && (
+          <>
+            <Button
+              type={"ghost"}
+              style={{
+                borderRadius: "4px",
+                height: "32px",
+                boxShadow: "rgba(0, 0, 0, 0.1) 0px 1px 2px 0px",
+                marginTop: "12px",
+              }}
+              onClick={() => {
+                navigate(`/courses/edit/${pid}`, { replace: true });
+              }}
+            >
+              수정
+            </Button>
+            <Button
+              type={"ghost"}
+              style={{
+                borderRadius: "4px",
+                height: "32px",
+                boxShadow: "rgba(0, 0, 0, 0.1) 0px 1px 2px 0px",
+                marginTop: "12px",
+              }}
+              onClick={() => {
+                if (window.confirm("정말 삭제하시겠습니까?") === true) {
+                  SyllabusApi.DSyllabus(syllabus._id)
+                    .then(() => {
                       alert(SUCCESS_MESSAGE);
-                      navigate("/courses");
+                      navigate("/courses#개설%20수업");
                     })
                     .catch((err) => {
                       alert(err.response.body.message);
                     });
-                }}
-              >
-                삭제
-              </Button>
-            </>
-          )}
-          <div style={{ height: "24px" }}></div>
-
-          {confirmStatusPopupActive && (
-            <Popup
-              setState={setConfirmStatusPopupActive}
-              title="승인 상태"
-              closeBtn
+                } else {
+                  return false;
+                }
+              }}
             >
-              <Table
-                type="object-array"
-                data={courseData.teachers}
-                header={[
-                  {
-                    text: "No",
-                    type: "text",
-                    key: "tableRowIndex",
-                    width: "48px",
-                    textAlign: "center",
-                  },
-                  {
-                    text: "멘토 ID",
-                    key: "userId",
-                    type: "text",
-                    textAlign: "center",
-                  },
-                  {
-                    text: "멘토 이름",
-                    key: "userName",
-                    type: "text",
-                    textAlign: "center",
-                  },
+              삭제
+            </Button>
+          </>
+        )}
+        <div style={{ height: "24px" }}></div>
+      </div>
+      {confirmStatusPopupActive && (
+        <Popup
+          setState={setConfirmStatusPopupActive}
+          title="승인 상태"
+          closeBtn
+        >
+          <Table
+            type="object-array"
+            data={syllabus.teachers}
+            header={[
+              {
+                text: "No",
+                type: "text",
+                key: "tableRowIndex",
+                width: "48px",
+                textAlign: "center",
+              },
+              {
+                text: "멘토 ID",
+                key: "userId",
+                type: "text",
+                textAlign: "center",
+              },
+              {
+                text: "멘토 이름",
+                key: "userName",
+                type: "text",
+                textAlign: "center",
+              },
 
-                  {
-                    text: "상태",
-                    key: "confirmed",
-                    width: "120px",
-                    textAlign: "center",
-                    type: "status",
-                    status: {
-                      false: { text: "미승인", color: "red" },
-                      true: { text: "승인됨", color: "green" },
+              {
+                text: "상태",
+                key: "confirmed",
+                width: "120px",
+                textAlign: "center",
+                type: "status",
+                status: {
+                  false: {
+                    text: "미승인",
+                    color: "red",
+                    onClick: (e: any) => {
+                      if (e._id === currentUser._id) {
+                        SyllabusApi.ConfirmSyllabus(syllabus?._id)
+                          .then(() => {
+                            alert(SUCCESS_MESSAGE);
+                            setIsLoading(true);
+                          })
+                          .catch((err) => {
+                            alert("failed to confirm");
+                          });
+                      }
                     },
                   },
-                ]}
-              />
-            </Popup>
-          )}
-        </>
-      </div>
+                  true: {
+                    text: "승인됨",
+                    color: "green",
+                    onClick: (e: any) => {
+                      if (e._id === currentUser._id) {
+                        EnrollmentApi.REnrolllments({
+                          syllabus: syllabus._id,
+                        }).then((enrollments: any[]) => {
+                          if (enrollments.length !== 0) {
+                            alert(
+                              "수강신청한 학생이 있으면 승인을 취소할 수 없습니다."
+                            );
+                          } else {
+                            SyllabusApi.UnconfirmSyllabus(syllabus?._id)
+                              .then((res) => {
+                                alert(SUCCESS_MESSAGE);
+                                setIsLoading(true);
+                              })
+                              .catch((err) => {
+                                alert("failed to unconfirm");
+                              });
+                          }
+                        });
+                      }
+                    },
+                  },
+                },
+              },
+            ]}
+          />
+        </Popup>
+      )}
     </>
   ) : (
     <Loading height={"calc(100vh - 55px)"} />
