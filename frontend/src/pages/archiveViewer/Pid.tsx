a496208d8e68128a066b796d3496e1fe8ba0536b
jessie129j@gmail.com <jessie129j@gmail.com>
2023-04-16 20:01:11 +0900

    feat: Create archive page for student #159

--- frontend/src/pages/archiveViewer/Pid.tsx
+++ frontend/src/pages/archiveViewer/Pid.tsx
@@ -1,26 +1,53 @@
 import { useAuth } from "contexts/authContext";
 import Navbar from "layout/navbar/Navbar";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import style from "style/pages/archive.module.scss";
+import { useEffect, useRef, useState } from "react";
 
 import ArrayView from "./tab/ArrayView";
 import ObjectView from "./tab/ObjectView";
+import Loading from "components/loading/Loading";
 
 type Props = {};
 
 const ArchiveField = (props: Props) => {
   const { pid } = useParams(); // archive label ex) 인적 사항
-  const { currentSchool } = useAuth();
+  const { currentSchool, currentRegistration, currentUser } = useAuth();
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    if (currentRegistration && pid) {
+      setIsLoading(true);
+    }
+  }, [currentRegistration, pid]);
+
+  useEffect(() => {
+    if (isLoading) {
+      if (
+        !currentRegistration ||
+        currentRegistration?.role !== "student" ||
+        !formArchive().authStudent ||
+        formArchive().authStudent === "undefined"
+      ) {
+        alert("접근 권한이 없습니다.");
+        navigate("/");
+      }
+      if (formArchive().authStudent === "view") {
+        setIsLoading(false);
+      }
+    }
+  }, [isLoading]);
 
   function formArchive() {
     return (
       currentSchool.formArchive?.filter((val: any) => {
         return val.label === pid;
-      })[0] ?? { fields: [] }
+      })[0] ?? { authStudent: "undefined", fields: [] }
     );
   }
 
-  return (
+  return !isLoading ? (
     <>
       <Navbar />
       <div className={style.section}>
@@ -29,6 +56,8 @@ const ArchiveField = (props: Props) => {
         {formArchive().dataType === "object" ? <ObjectView /> : <ArrayView />}
       </div>
     </>
+  ) : (
+    <Loading height={"calc(100vh - 55px)"} />
   );
 };
 
