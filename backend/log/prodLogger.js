2bce6ae6b7b3efcda26c14e00d8cf99dcc11075f
jessie129j@gmail.com <jessie129j@gmail.com>
2023-04-24 17:55:13 +0900

    fix: Update production loger #163

--- backend/log/prodLogger.js
+++ backend/log/prodLogger.js
@@ -6,16 +6,20 @@ const S3StreamLogger = require("s3-streamlogger-daily").S3StreamLogger;
 const strftime = require("strftime");
 
 const strftimeKOR = strftime.timezone("+0900");
-const time_data = strftimeKOR("%F", new Date());
 
 /* prodLogger */
-const stream = (level = "") =>
-  new S3StreamLogger({
+const stream = (level = "") => {
+  const time_data = strftimeKOR("%F %T", new Date());
+
+  return new S3StreamLogger({
     bucket: "altsis-logs",
     access_key_id: process.env.s3_accessKeyId2,
     secret_access_key: process.env.s3_secretAccessKey2,
     name_format: `${time_data}${level !== "" ? "." + level : ""}.log`,
+    rotate_every: "day",
+    max_file_size: 2000000000, // 2gb
   });
+};
 
 const prodLogger = winston.createLogger({
   level: "http",
