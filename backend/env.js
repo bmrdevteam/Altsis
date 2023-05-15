90e0e6256b1be1d9db36294a1eede3b97e1639c3
jessie129j@gmail.com <jessie129j@gmail.com>
2023-05-15 10:16:29 +0900

    feat: Add local mode #184

--- backend/env.js
+++ backend/env.js
@@ -1,3 +1,11 @@
 import dotenv from "dotenv";
+import { resolve } from "path";
+import { fileURLToPath } from "url";
 
-dotenv.config();
+console.log("NODE_ENV: ", process.env.NODE_ENV);
+if (process.env.NODE_ENV?.trim() === "local") {
+  const __dirname = fileURLToPath(new URL(".", import.meta.url));
+  dotenv.config({ path: resolve(__dirname, "./.env.local") });
+} else {
+  dotenv.config();
+}
