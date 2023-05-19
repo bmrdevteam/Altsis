24b1b210ec3ad66f7a7a384b3ad8a05e944a4492
jessie129j@gmail.com <jessie129j@gmail.com>
2023-05-19 19:07:28 +0900

    fix: Re-conduct performance test #187

---
+++ backend/tests/enrollment/src/routes/test.js
@@ -0,0 +1,15 @@
+/**
+ * Copy and paste this to routes>test.js
+ *
+ */
+router.get("/clean", async (req, res) => {
+  try {
+    await Enrollment("garisan").deleteMany({});
+    await Syllabus("garisan").updateMany({}, { count: 0 });
+
+    return res.status(200).send({ message: "done" });
+  } catch (err) {
+    logger.error(err.message);
+    return res.status(500).send({ message: err.message });
+  }
+});
