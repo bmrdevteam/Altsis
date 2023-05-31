c95a54d856fba59952dfae280d130536b8623c1c
jessie129j@gmail.com <jessie129j@gmail.com>
2023-06-01 00:41:34 +0900

    fix: Block cleaning redis socket data in non-production env #201

--- backend/_database/redis/index.js
+++ backend/_database/redis/index.js
@@ -30,20 +30,23 @@ client.on("error", function (err) {
 });
 
 client.on("ready", async () => {
-  // clean up "io/notification/*" data
-  const [keys1, keys2] = await Promise.all([
-    client.v4.hGetAll("io/notification/sid-user"),
-    client.v4.hGetAll("io/notification/user-sidList"),
-  ]);
+  // clean up "io/notification/*" data (only in production)
 
-  await Promise.all([
-    Object.keys(keys1).forEach((key) =>
-      client.hDel("io/notification/sid-user", key)
-    ),
-    Object.keys(keys2).forEach((key) =>
-      client.hDel("io/notification/user-sidList", key)
-    ),
-  ]);
+  if (process.env.NODE_ENV?.trim() === "production") {
+    const [keys1, keys2] = await Promise.all([
+      client.v4.hGetAll("io/notification/sid-user"),
+      client.v4.hGetAll("io/notification/user-sidList"),
+    ]);
+
+    await Promise.all([
+      Object.keys(keys1).forEach((key) =>
+        client.hDel("io/notification/sid-user", key)
+      ),
+      Object.keys(keys2).forEach((key) =>
+        client.hDel("io/notification/user-sidList", key)
+      ),
+    ]);
+  }
 
   const text = "✅ Redis is connected";
   console.log(text);
