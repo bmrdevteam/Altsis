37bc312b3040c78b3c98b50c31ad3b3d405fcdbc
jessie129j@gmail.com <jessie129j@gmail.com>
2023-06-04 21:34:47 +0900

    fix: Order APIs #201

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -18,37 +18,37 @@ router.post("/login/local", forceNotLoggedIn, users.loginLocal);
 router.post("/login/google", forceNotLoggedIn, users.loginGoogle);
 router.get("/logout", isLoggedIn, users.logout);
 
-// ___________ create _____________
+// ___________ Create _____________
 router.post("/", isAdmin, users.create);
 
-// ___________ find _____________
-router.get("/current", isLoggedIn, users.current);
-router.get("/:_id/profile", isLoggedIn, users.findProfile);
+// ___________ Find _____________
 router.get("/", isOwAdManager, users.findUsers);
+router.get("/current", isLoggedIn, users.current);
 router.get("/:_id", isOwAdManager, users.findUser);
+router.get("/:_id/profile", isLoggedIn, users.findProfile);
 
-//
-
-// ___________ update(onself) _____________
+// ___________ Update (by user) _____________
 
 router.put("/profile", isLoggedIn, profile.update);
-
 router.put("/calendar", isLoggedIn, users.updateCalendar);
 
-// ___________ By Admin _____________
+// ___________ Update (by user & admin) _____________
 
-router.put("/schools/bulk", isAdmin, users.updateSchoolsBulk); //
 router.put("/:_id/password", isLoggedIn, users.updatePassword);
-router.put("/:_id/google", isAdmin, users.connectGoogleAuth);
-router.delete("/:_id/google", isAdmin, users.disconnectGoogleAuth);
-router.put("/:_id/auth", isAdmin, users.updateAuth);
 router.put("/:_id/email", isLoggedIn, users.updateEmail);
 router.put("/:_id/tel", isLoggedIn, users.updateTel);
+
+// ___________ Update (by admin) _____________
+
+router.put("/:_id/auth", isAdmin, users.updateAuth);
+
+router.put("/:_id/google", isAdmin, users.connectGoogleAuth);
+router.delete("/:_id/google", isAdmin, users.disconnectGoogleAuth);
 
 router.post("/:_id/schools", isAdmin, users.registerSchool);
 router.delete("/:_id/schools", isAdmin, users.deregisterSchool);
 
-// ___________ delete _____________
+// ___________ Delete _____________
 router.delete("/:_id", isAdmin, users.remove);
 
 export { router };
