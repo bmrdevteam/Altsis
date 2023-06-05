5118cddbd6694cbf4d6c5b8736a4be3b201e1b06
jessie129j@gmail.com <jessie129j@gmail.com>
2023-06-05 16:16:27 +0900

    feat: Add hide/showEnrollmentFromCalendar APIs #207

--- backend/routes/enrollments.js
+++ backend/routes/enrollments.js
@@ -21,6 +21,10 @@ router.put("/:_id/evaluation2", isLoggedIn, enrollments.updateEvaluation2);
 // 수강 정보에 memo 작성 (only student can access)
 router.put("/:_id/memo", isLoggedIn, enrollments.updateMemo);
 
+// 수강 정보 캘린더에서 조회/미조회
+router.put("/:_id/hide", isLoggedIn, enrollments.hideFromCalendar);
+router.put("/:_id/show", isLoggedIn, enrollments.showOnCalendar);
+
 // param _id -> only himself/herself can access
 // query season&userId -> 유저의 수강 정보 (-evaluation)
 // query syllabus -> syllabus 수강하는 학생 목록 (-evaluation)
