865dfa522a838f2a291edf2373707e2b1ac6cd26
jessie129j@gmail.com <jessie129j@gmail.com>
2023-06-15 17:31:03 +0900

    fix: fix CUploadFileArchive API bug #229

--- backend/routes/files.js
+++ backend/routes/files.js
@@ -1,13 +1,13 @@
 import express from "express";
 const router = express.Router();
 import * as files from "../controllers/files.js";
-import { isAdManager, isOwner, isLoggedIn } from "../middleware/auth.js";
+import { isOwner, isLoggedIn } from "../middleware/auth.js";
 
 //=================================
 //             File
 //=================================
 
-router.post("/archive", isAdManager, files.uploadArchive);
+router.post("/archive", isLoggedIn, files.uploadArchive);
 router.get("/archive/signed", isLoggedIn, files.signArchive);
 
 router.get("/document/signed", isLoggedIn, files.signDocument);
