af807e083943273f446acb7b4eb77ecc9bcab5a0
jessie129j@gmail.com <jessie129j@gmail.com>
2023-09-01 15:14:12 +0900

    fix: set permission when creating registration #270

--- backend/services/seasons.js
+++ backend/services/seasons.js
@@ -27,3 +27,38 @@ export const getSeasonSubRecord = (seasonRecord) => {
     formEvaluation: seasonRecord.formEvaluation,
   };
 };
+
+/**
+ * @param {Object} permission
+ * @param {boolean} permission.teacher
+ * @param {boolean} permission.student
+ * @param {{userId:string,isAllowed:boolean}[]} permission.exceptions
+ * @param {string} userId
+ * @param {"teacher"|"student"} role
+ */
+const _hasPermission = (permission, userId, role) => {
+  for (let exception of permission.exceptions) {
+    if (exception.userId === userId) {
+      return exception.isAllowed;
+    }
+  }
+  return permission[role];
+};
+
+/**
+ * @param {"syllabus"|"enrollment"|"evaluation"} type
+ * @param {{permissionSyllabusV2,permissionEnrollmentV2,permissionEvaluationV2}} seasonRecord
+ * @param {string} userId
+ * @param {"teacher"|"student"} role
+ */
+export const hasPermission = (type, seasonRecord, userId, role) => {
+  switch (type) {
+    case "syllabus":
+      return _hasPermission(seasonRecord.permissionSyllabusV2, userId, role);
+    case "enrollment":
+      return _hasPermission(seasonRecord.permissionEnrollmentV2, userId, role);
+    case "evaluation":
+      return _hasPermission(seasonRecord.permissionEvaluationV2, userId, role);
+  }
+  return false;
+};
